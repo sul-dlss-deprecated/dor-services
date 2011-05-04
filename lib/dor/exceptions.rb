4936ec10108b00472d98fa19907a5dcf761bc216
Michael Klein <mbklein@gmail.com>
2011-05-04 11:44:00 -0700

    Updated Dor::DuplicatedIdError to include existing PID

--- lib/dor/exceptions.rb
+++ lib/dor/exceptions.rb
@@ -2,6 +2,12 @@ module Dor
   
   class Exception < ::Exception; end
   class ParameterError < Exception; end
-  class DuplicateIdError < Exception; end
+  class DuplicateIdError < Exception
+    attr_reader :pid
+    
+    def initialize(pid)
+      @pid = pid
+    end
+  end
   
 end
