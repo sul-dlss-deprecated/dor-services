d613bfaab9d1dba7f4649b2325a6e050766c576a
Willy Mene <wmene@stanford.edu>
2011-06-14 10:15:58 -0700

    Fixed item spec so that suri does not get called

--- spec/dor/item_spec.rb
+++ spec/dor/item_spec.rb
@@ -2,6 +2,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 require 'equivalent-xml'
 
 describe Dor::Item do
+  
   before :all do
     @saved_configuration = Dor::Config.to_hash
     Dor::Config.configure do
@@ -9,10 +10,13 @@ describe Dor::Item do
       gsearch.url "http://solr.edu"
       fedora.url "http://fedora.edu"
     end
-    
+
     Rails.stub_chain(:logger, :error)
     ActiveFedora::SolrService.register(Dor::Config.gsearch.url)
     Fedora::Repository.register(Dor::Config.fedora.url)
+  end
+  
+  before(:each) do
     Fedora::Repository.stub!(:instance).and_return(stub('frepo').as_null_object)
   end
   
@@ -42,49 +46,52 @@ describe Dor::Item do
   describe "#public_xml" do
     
     context "produces xml with" do
-        b = Dor::Item.new
+        before(:each) do
+          @b = Dor::Item.new
+          @b.stub!(:pid).and_return('druid:123456')
 
-        mods = <<-EOXML
-          <mods:mods xmlns:mods="http://www.loc.gov/mods/v3"
-                     xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-                     version="3.3"
-                     xsi:schemaLocation="http://www.loc.gov/mods/v3 http://cosimo.stanford.edu/standards/mods/v3/mods-3-3.xsd"/>
-        EOXML
-        descmd_ds = ActiveFedora::NokogiriDatastream.new(:dsid=> 'descMetadata', :blob => mods)
-        b.add_datastream(descmd_ds)
+          mods = <<-EOXML
+            <mods:mods xmlns:mods="http://www.loc.gov/mods/v3"
+                       xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                       version="3.3"
+                       xsi:schemaLocation="http://www.loc.gov/mods/v3 http://cosimo.stanford.edu/standards/mods/v3/mods-3-3.xsd"/>
+          EOXML
+          descmd_ds = ActiveFedora::NokogiriDatastream.new(:dsid=> 'descMetadata', :blob => mods)
+          @b.add_datastream(descmd_ds)
 
-        id_ds = IdentityMetadataDS.new(:dsid=> 'identityMetadata', :blob => '<identityMetadata/>')
-        b.add_datastream(id_ds)
-        cm_ds = ActiveFedora::NokogiriDatastream.new(:dsid=> 'contentMetadata', :blob => '<contentMetadata/>')
-        b.add_datastream(cm_ds)
-        r_ds = ActiveFedora::NokogiriDatastream.new(:dsid=> 'rightsMetadata', :blob => '<rightsMetadata/>')
-        b.add_datastream(r_ds)
-        p_xml = Nokogiri::XML(b.public_xml)
-
+          id_ds = IdentityMetadataDS.new(:dsid=> 'identityMetadata', :blob => '<identityMetadata/>')
+          @b.add_datastream(id_ds)
+          cm_ds = ActiveFedora::NokogiriDatastream.new(:dsid=> 'contentMetadata', :blob => '<contentMetadata/>')
+          @b.add_datastream(cm_ds)
+          r_ds = ActiveFedora::NokogiriDatastream.new(:dsid=> 'rightsMetadata', :blob => '<rightsMetadata/>')
+          @b.add_datastream(r_ds)
+          @p_xml = Nokogiri::XML(@b.public_xml)
+        end
+        
        it "an id attribute" do
-         p_xml.at_xpath('/publicObject/@id').value.should =~ /^druid:/
+         @p_xml.at_xpath('/publicObject/@id').value.should =~ /^druid:123456/
        end
 
        it "identityMetadata" do
-         p_xml.at_xpath('/publicObject/identityMetadata').should be
+         @p_xml.at_xpath('/publicObject/identityMetadata').should be
        end
 
        it "contentMetadata" do
-         p_xml.at_xpath('/publicObject/contentMetadata').should be
+         @p_xml.at_xpath('/publicObject/contentMetadata').should be
        end
 
        it "rightsMetadata" do
-         p_xml.at_xpath('/publicObject/rightsMetadata').should be
+         @p_xml.at_xpath('/publicObject/rightsMetadata').should be
        end
 
        it "generated dublin core" do         
-         p_xml.at_xpath('/publicObject/oai_dc:dc', 'oai_dc' => 'http://www.openarchives.org/OAI/2.0/oai_dc/').should be
+         @p_xml.at_xpath('/publicObject/oai_dc:dc', 'oai_dc' => 'http://www.openarchives.org/OAI/2.0/oai_dc/').should be
        end
        
        it "clones of the content of the other datastreams, keeping the originals in tact" do
-         b.datastreams['identityMetadata'].ng_xml.at_xpath("/identityMetadata").should be
-         b.datastreams['contentMetadata'].ng_xml.at_xpath("/contentMetadata").should be
-         b.datastreams['rightsMetadata'].ng_xml.at_xpath("/rightsMetadata").should be
+         @b.datastreams['identityMetadata'].ng_xml.at_xpath("/identityMetadata").should be
+         @b.datastreams['contentMetadata'].ng_xml.at_xpath("/contentMetadata").should be
+         @b.datastreams['rightsMetadata'].ng_xml.at_xpath("/rightsMetadata").should be
        end
     end
   
