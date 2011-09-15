b2a1887d58c726f93f28084555cd9ec176c22666
Michael Klein <mbklein@gmail.com>
2011-09-15 11:02:44 -0700

    Remove unnecessary cruft from WorkflowDS

--- lib/datastreams/workflow_ds.rb
+++ lib/datastreams/workflow_ds.rb
@@ -12,43 +12,5 @@ class WorkflowDs < ActiveFedora::NokogiriDatastream
       t.attempts(:path=>{:attribute=>"attempts"})
     }
   end
-  
-  def self.xml_template
-    builder = Nokogiri::XML::Builder.new do |xml|
-      xml.workflow {
-          xml.process
-      }   
-    end
-
-    return builder.doc
-  end
-  
-  def to_solr(solr_doc = Solr::Document.new)
-    workflow_name = self.term_values(:workflowId).first
-    solr_doc << Solr::Field.new(:workflow_display => workflow_name)
-    solr_doc << Solr::Field.new(:workflow_facet => workflow_name)
     
-    self.find_by_terms(:process).each { |process|
-      process_name = process['name']
-      process_status = process['status']
-      process_timestamp = DateTime.parse(process['datetime']).utc.to_time.xmlschema
-      combined_process_name = "#{workflow_name}_#{process_name}"
-      solr_doc << Solr::Field.new(:workflow_step_facet => process_name)
-#      solr_doc << Solr::Field.new(:qualified_workflow_step_facet => combined_process_name)
-      solr_doc << Solr::Field.new(:workflow_step_status_facet => process_status)
-      
-      solr_doc << Solr::Field.new(:"#{workflow_name}_process_facet" => process_name)
-      
-      solr_doc << Solr::Field.new(:"#{process_name}_status_facet" => process_status)
-      solr_doc << Solr::Field.new(:"#{combined_process_name}_status_facet" => process_status)
-      solr_doc << Solr::Field.new(:"#{combined_process_name}_status_dt" => process_timestamp)
-      
-      solr_doc << Solr::Field.new(:"workstep_status_#{process_status}_facet" => process_name)
-      
-      # THIS IS A TEMPORARY KLUDGE TO GET STUFF TO SHOW UP IN HYDRANGEA
-      solr_doc << Solr::Field.new(:discover_access_group_t => 'public')
-      solr_doc << Solr::Field.new(:read_access_group_t => 'public')
-    }
-    return solr_doc
-  end
 end
