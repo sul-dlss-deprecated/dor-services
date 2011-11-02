ef6f0a6c992cde9b92acfdad3317873b560d7012
Michael Klein <mbklein@gmail.com>
2011-11-02 16:52:03 -0700

    Clean up loading; make entire gem reloadable via Dor.reload!

--- lib/dor-services.rb
+++ lib/dor-services.rb
@@ -1,19 +1,46 @@
-require 'dor/config'
-require 'dor/exceptions'
+module Dor
+  Dependencies = [
+    'dor/config',
+    'dor/exceptions',
 
-# ActiveFedora Classes
-require 'dor/base'
-require 'dor/item'
-require 'dor/admin_policy_object'
-require 'dor/workflow_object'
+    # ActiveFedora Classes
+    'dor/base',
+    'dor/item',
+    'dor/admin_policy_object',
+    'dor/workflow_object',
 
-# Services
-require 'dor/metadata_service'
-require 'dor/registration_service'
-require 'dor/suri_service'
-require 'dor/workflow_service'
-require 'dor/digital_stacks_service'
-require 'dor/druid_utils'
-require 'dor/sdr_ingest_service'
-require 'dor/cleanup_service'
-require 'dor/provenance_metadata_service'
+    # Services
+    'dor/search_service',
+    'dor/metadata_service',
+    'dor/registration_service',
+    'dor/suri_service',
+    'dor/workflow_service',
+    'dor/digital_stacks_service',
+    'dor/druid_utils',
+    'dor/sdr_ingest_service',
+    'dor/cleanup_service',
+    'dor/provenance_metadata_service'
+  ]
+  Dependencies.each { |f| require f }
+  
+  class << self
+    
+    def configure *args, &block
+      Dor::Config.configure *args, &block
+    end
+    
+    def reload!
+      configuration = Dor::Config.to_hash
+      temp_v = $-v
+      $-v = nil
+      begin
+        Dependencies.collect { |f| load File.join(File.dirname(__FILE__), "#{f}.rb") }
+      ensure
+        $-v = temp_v
+      end
+      Dor::Config.configure configuration
+      Dor
+    end
+    
+  end
+end
