55cc0a7ef6bd15d03955f39ee1be44f9aa2ec0ce
Michael Klein <mbklein@gmail.com>
2012-04-18 20:44:43 -0700

    Add workflow name and archive totals to WorkflowObject index

--- lib/dor/models/workflow_object.rb
+++ lib/dor/models/workflow_object.rb
@@ -3,12 +3,13 @@ require 'dor/datastreams/workflow_definition_ds'
 module Dor
   class WorkflowObject < ::ActiveFedora::Base
     include Identifiable
+    include SolrDocHelper
     
     has_object_type 'workflow'
     has_metadata :name => "workflowDefinition", :type => Dor::WorkflowDefinitionDs, :label => 'Workflow Definition'
 
     def self.find_by_name(name, opts={})
-      Dor.find_all(%{objectType_t:"#{self.object_type}" title_facet:"#{name}"}, opts).first
+      Dor.find_all(%{objectType_t:"#{self.object_type}" workflow_name_s:"#{name}"}, opts).first
     end
     
     def definition
@@ -19,5 +20,13 @@ module Dor
       self.definition.graph *args
     end
     
+    def to_solr solr_doc=Hash.new, *args
+      super solr_doc, *args
+      client = Dor::WorkflowService.workflow_resource
+      xml = client["workflow_archive?repository=#{definition.repo}&workflow=#{definition.name}&count-only=true"].get
+      count = Nokogiri::XML(xml).at_xpath('/objects/@count').value
+      add_solr_value(solr_doc,"#{definition.name}_archived",count,:integer,[:displayable])
+      solr_doc
+    end
   end
 end
