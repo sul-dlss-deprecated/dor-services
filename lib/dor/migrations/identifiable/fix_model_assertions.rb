3e3f767ae7a908f5611fd7c701978de2457e03b2
Michael Klein <mbklein@gmail.com>
2012-05-04 10:37:16 -0700

    Reinforce idempotence of model assertion migration

--- lib/dor/migrations/identifiable/fix_model_assertions.rb
+++ lib/dor/migrations/identifiable/fix_model_assertions.rb
@@ -1,10 +1,13 @@
 Dor::Identifiable.on_upgrade '3.6.1', 'Assert correct models' do |obj|
+  applied = false
   if obj.relationships.any? { |r| r.predicate.to_s == 'info:fedora/fedora-system:def/model#hasModel' && r.object.to_s == 'info:fedora/hydra:commonMetadata' }
     obj.remove_relationship :has_model, 'info:fedora/hydra:commonMetadata'
+    applied = true
   end
   
   unless obj.relationships.predicates.any? { |p| p.to_s == 'info:fedora/fedora-system:def/model#hasModel' }
     obj.assert_content_model
+    applied = true
   end
-  obj.rels_ext.dirty?
+  applied
 end
