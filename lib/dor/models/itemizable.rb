1e797ed67204d83cad52bf9b22e33121bcf66bd4
Michael Klein <mbklein@gmail.com>
2012-06-07 14:51:56 -0700

    Itemizable#get_content_diff

--- lib/dor/models/itemizable.rb
+++ lib/dor/models/itemizable.rb
@@ -15,5 +15,20 @@ module Dor
       end
     end
     
+    def get_content_diff(version=nil)
+      basename = version.nil? ? 'cm_inv_diff.xml' : "'cm_inv_diff.#{version}.xml'"
+      diff_path = File.join(DruidTools::Druid.new(self.pid,Dor::Config.stacks.local_workspace_root).temp_dir,basename)
+      if File.exists? diff_path
+        File.read(diff_path)
+      else
+        sdr_client = Dor::Config.sdr.rest_client
+        current_content = self.datastreams['contentMetadata'].content
+        url = "objects/#{self.pid}/cm-inv-diff?version=#{version.to_s}"
+        response = sdr_client[url].post(current_content, :content_type => 'application/xml')
+        File.open(diff_path,'w') { |f| f.write(response) }
+        response
+      end
+    end
+    
   end
 end
