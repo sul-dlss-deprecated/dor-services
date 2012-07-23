b2ee28a8253cb0ef80d2edb080c78ac1d38e44ba
Willy Mene <wmene@stanford.edu>
2012-07-23 16:20:14 -0700

    Remove version as an optional param to workflow methods.
    Change doc stype to yard

--- lib/dor/services/workflow_service.rb
+++ lib/dor/services/workflow_service.rb
@@ -15,12 +15,12 @@ module Dor
       # Returns true on success.  Caller must handle any exceptions
       #
       # == Parameters
-      # - <b>repo</b> - The repository the object resides in.  The service recoginzes "dor" and "sdr" at the moment
-      # - <b>druid</b> - The id of the object
-      # - <b>workflow_name</b> - The name of the workflow you want to create
-      # - <b>wf_xml</b> - The xml that represents the workflow
-      # - <B>opts</b> - Options Hash where you can set
-      #       :create_ds - If true, a workflow datastream will be created in Fedora.  Set to false if you do not want a datastream to be created
+      # @param [String] repo The repository the object resides in.  The service recoginzes "dor" and "sdr" at the moment
+      # @param [String] druid The id of the object
+      # @param [String] workflow_name The name of the workflow you want to create
+      # @param [String] wf_xml The xml that represents the workflow
+      # @param [Hash] opts optional params
+      # @option opts [Boolean] :create_ds if true, a workflow datastream will be created in Fedora.  Set to false if you do not want a datastream to be created
       #   If you do not pass in an <b>opts</b> Hash, then :create_ds is set to true by default
       # 
       def create_workflow(repo, druid, workflow_name, wf_xml, opts = {:create_ds => true})
@@ -32,23 +32,20 @@ module Dor
       # Updates the status of one step in a workflow.      
       # Returns true on success.  Caller must handle any exceptions
       #
-      # == Required Parameters
       # @param [String] repo The repository the object resides in.  The service recoginzes "dor" and "sdr" at the moment
       # @param [String] druid The id of the object
       # @param [String] workflow_name The name of the workflow 
       # @param [String] status The status that you want to set.  Typical statuses are 'waiting', 'completed', 'error', but could be any string
-      # 
-      # == Optional Parameters
-      # - <b>:elapsed</b> - The number of seconds it took to complete this step. Can have a decimal.  Is set to 0 if not passed in.
-      # - <b>:lifecycle</b> - Bookeeping label for this particular workflow step.  Examples are: 'registered', 'shelved'
-      # - <b>:version</b> - Version number of the object
-      # - <b>:note</b> - Any kind of string annotation that you want to attach to the workflow
+      # @param [Hash] opts optional values for the workflow step
+      # @option opts [Float] :elapsed The number of seconds it took to complete this step. Can have a decimal.  Is set to 0 if not passed in.
+      # @option opts [String] :lifecycle Bookeeping label for this particular workflow step.  Examples are: 'registered', 'shelved'
+      # @option opts [String] :note Any kind of string annotation that you want to attach to the workflow
       # == Http Call
       # The method does an HTTP PUT to the URL defined in Dor::WF_URI.  As an example:
       #   PUT "/dor/objects/pid:123/workflows/GoogleScannedWF/convert"
       #   <process name=\"convert\" status=\"completed\" />"
       def update_workflow_status(repo, druid, workflow, process, status, opts = {}) #elapsed = 0, lifecycle = nil)
-        opts = {:elapsed => 0, :lifecycle => nil, :version => 1, :note => nil}.merge!(opts)
+        opts = {:elapsed => 0, :lifecycle => nil, :note => nil}.merge!(opts)
         opts[:elapsed] = opts[:elapsed].to_s
         xml = create_process_xml({:name => process, :status => status}.merge!(opts))
         workflow_resource["#{repo}/objects/#{druid}/workflows/#{workflow}/#{process}"].put(xml, :content_type => 'application/xml')
@@ -80,14 +77,13 @@ module Dor
       # @param [String] error_msg The error message.  Ideally, this is a brief message describing the error
       # @param [Hash] opts optional values for the workflow step
       # @option opts [String] :error_txt A slot to hold more information about the error, like a full stacktrace
-      # @option opts [Integer] :version version number of the object
       #
       # == Http Call
       # The method does an HTTP PUT to the URL defined in Dor::WF_URI.  As an example:
       #   PUT "/dor/objects/pid:123/workflows/GoogleScannedWF/convert"
       #   <process name=\"convert\" status=\"error\" />"
       def update_workflow_error_status(repo, druid, workflow, process, error_msg, opts = {})
-        opts = {:error_txt => nil, :version => 1}.merge!(opts)
+        opts = {:error_txt => nil}.merge!(opts)
         xml = create_process_xml({:name => process, :status => 'error', :errorMessage => error_msg}.merge!(opts))
         workflow_resource["#{repo}/objects/#{druid}/workflows/#{workflow}/#{process}"].put(xml, :content_type => 'application/xml')
         return true
