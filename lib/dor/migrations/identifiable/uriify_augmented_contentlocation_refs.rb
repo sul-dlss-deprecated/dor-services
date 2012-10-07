a65f3a6ad80857e870a076aab67853934ad2532d
Chris Beer <cabeer@stanford.edu>
2012-10-06 21:17:01 -0700

    add remediation task to fix up content-augmented dsLocations

---
+++ lib/dor/migrations/identifiable/uriify_augmented_contentlocation_refs.rb
@@ -0,0 +1,18 @@
+Dor::Identifiable.on_upgrade '3.6.2.2', 'Fix up invalid URIs in content-augmented datastreams' do |obj|
+  bad_content_location_uri = begin
+    URI.parse(obj.send("content-augmented").dsLocation)
+    false
+  rescue URI::InvalidURIError
+    true
+  rescue 
+    false
+  end
+
+  next unless bad_content_location_uri
+
+  parts = obj.send("content-augmented").dsLocation.split('/')
+  parts[parts.length - 1] = URI.escape(parts.last)
+  obj.send("content-augmented").dsLocation = parts.join('/')
+
+  obj.send("content-augmented").save
+end
