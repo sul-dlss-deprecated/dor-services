4ebf28b871828e3b3d30fcd34ab587ec0c7f0e5c
Willy Mene <wmene@stanford.edu>
2013-04-03 15:08:46 -0700

    Add 20% visibility fields to embargoMetadata

--- lib/dor/datastreams/embargo_metadata_ds.rb
+++ lib/dor/datastreams/embargo_metadata_ds.rb
@@ -1,23 +1,27 @@
 module Dor
 class EmbargoMetadataDS < ActiveFedora::NokogiriDatastream
   include SolrDocHelper
-  
+
   before_create :ensure_non_versionable
 
   set_terminology do |t|
     t.root(:path => "embargoMetadata")
     t.status(:index_as => [:searchable, :facetable])
-    t.release_date(:path => "releaseDate")#, :data_type => :date)
+    t.release_date(:path => "releaseDate")
     t.release_access(:path => "releaseAccess")
+    t.twenty_pct_status( :path => "twentyPctVisibilityStatus", :index_as => [:searchable, :facetable])
+    t.twenty_pct_release_date(:path => "twentyPctVisibilityReleaseDate")
   end
-  
-  # Default EmbargoMetadataDS xml 
+
+  # Default EmbargoMetadataDS xml
   def self.xml_template
     builder = Nokogiri::XML::Builder.new do |xml|
       xml.embargoMetadata {
         xml.status
         xml.releaseDate
         xml.releaseAccess
+        xml.twentyPctVisibilityStatus
+        xml.twentyPctVisibilityReleaseDate
       }
     end
     return builder.doc
@@ -26,28 +30,29 @@ class EmbargoMetadataDS < ActiveFedora::NokogiriDatastream
   def to_solr solr_doc = {}, *args
     super
     add_solr_value(solr_doc, 'embargo_release_date', self.release_date.utc.strftime('%FT%TZ') , :date, [:searchable]) rescue nil
+    add_solr_value(solr_doc, 'twenty_pct_visibility_release_date', self.twenty_pct_release_date.utc.strftime('%FT%TZ') , :date, [:searchable]) rescue nil
 
     solr_doc
   end
-  
+
   def ensure_non_versionable
     self.versionable = "false"
   end
-  
+
   #################################################################################
   # Convenience methods to get and set embargo properties
-  # Hides complexity/verbosity of OM TermOperators for simple, non-repeating values 
-  #################################################################################  
+  # Hides complexity/verbosity of OM TermOperators for simple, non-repeating values
+  #################################################################################
 
   def status=(new_status)
     update_values([:status] => new_status)
     self.dirty = true
   end
-  
+
   def status
     term_values(:status).first
   end
-  
+
   # Sets the release date.  Converts the date to beginning-of-day, UTC to help with Solr indexing
   # @param [Time] rd A Time object represeting the release date.  By default, it is set to now
   def release_date=(rd=Time.now)
@@ -55,29 +60,56 @@ class EmbargoMetadataDS < ActiveFedora::NokogiriDatastream
     	self.content=self.ng_xml.to_s
 		self.dirty = true
   end
-  
+
   # Current releaseDate value
   # @return [Time]
   def release_date
     Time.parse(term_values(:release_date).first)
   end
-  
+
+  def twenty_pct_status=(new_status)
+    update_values([:twenty_pct_status] => new_status)
+    self.dirty = true
+  end
+
+  def twenty_pct_status
+    term_values(:twenty_pct_status).first
+  end
+
+  # Sets the 20% visibility release date.  Converts the date to beginning-of-day, UTC to help with Solr indexing
+  # @param [Time] rd A Time object represeting the release date.  By default, it is set to now
+  def twenty_pct_release_date=(rd=Time.now)
+    update_values([:twenty_pct_release_date] => rd.beginning_of_day.utc.xmlschema)
+    self.dirty = true
+  end
+
+  # Current twentyPctVisibilityReleaseDate value
+  # @return [Time]
+  def twenty_pct_release_date
+    Time.parse(term_values(:twenty_pct_release_date).first)
+  end
+
   # @return [Nokogiri::XML::Element] The releaseAccess node
   def release_access_node
     find_by_terms(:release_access).first
   end
-  
+
+  # @return [Nokogiri::XML::Element] The releaseAccess node
+  def release_access_node
+    find_by_terms(:release_access).first
+  end
+
   # Sets the embargaAccess node
   # @param [Nokogiri::XML::Document] new_doc Document that will replace the existing releaseAccess node
   def release_access_node=(new_doc)
     if(new_doc.root.name != 'releaseAccess')
       raise "Trying to replace releaseAccess with a non-releaseAccess document"
     end
-    
+
     term_value_delete(:select => '//embargoMetadata/releaseAccess')
     ng_xml.root.add_child(new_doc.root.clone)
     self.dirty = true
   end
-  
+
 end
 end
--- spec/datastreams/embargo_metadata_spec.rb
+++ spec/datastreams/embargo_metadata_spec.rb
@@ -5,86 +5,92 @@ require 'dor/datastreams/embargo_metadata_ds'
 
 describe Dor::EmbargoMetadataDS do
   context "Marshalling to and from a Fedora Datastream" do
-    before(:each) do 
-      @dsxml =<<-EOF
-            <embargoMetadata>
-            	<status>embargoed</status>
-            	<releaseDate>2011-10-12T15:47:52-07:00</releaseDate>
-            	<releaseAccess>
-            		<access type="discover">
-            			<machine>
-            				<world />
-            			</machine>
-            		</access>
-            		<access type="read">
-            			<machine>
-            				<world />
-            			</machine>
-            		</access>
-            	</releaseAccess>
-            </embargoMetadata>
-      EOF
-    end
-    
+    let(:dsxml) { <<-EOF
+          <embargoMetadata>
+          	<status>embargoed</status>
+          	<releaseDate>2011-10-12T15:47:52-07:00</releaseDate>
+          	<twentyPctVisibilityStatus>released</twentyPctVisibilityStatus>
+          	<twentyPctVisibilityReleaseDate>2016-10-12T15:47:52-07:00</twentyPctVisibilityReleaseDate>
+          	<releaseAccess>
+          		<access type="discover">
+          			<machine>
+          				<world />
+          			</machine>
+          		</access>
+          		<access type="read">
+          			<machine>
+          				<world />
+          			</machine>
+          		</access>
+          	</releaseAccess>
+          </embargoMetadata>
+    EOF
+    }
+
     it "creates itself from xml" do
-      ds = Dor::EmbargoMetadataDS.from_xml(@dsxml)
+      ds = Dor::EmbargoMetadataDS.from_xml(dsxml)
       ds.term_values(:status).should == ["embargoed"]
       ds.term_values(:release_date).should == ["2011-10-12T15:47:52-07:00"]
+      ds.term_values(:twenty_pct_status).should == ["released"]
+      ds.term_values(:twenty_pct_release_date).should == ["2016-10-12T15:47:52-07:00"]
       ds.find_by_terms(:release_access).class.should == Nokogiri::XML::NodeSet
     end
-        
+
     it "creates a simple default with #new" do
       emb_xml = <<-EOF
       <embargoMetadata>
       	<status/>
       	<releaseDate/>
       	<releaseAccess/>
+      	<twentyPctVisibilityStatus/>
+        <twentyPctVisibilityReleaseDate/>
       </embargoMetadata>
       EOF
-      
+
       ds = Dor::EmbargoMetadataDS.new nil, 'embargoMetadata'
       ds.to_xml.should be_equivalent_to(emb_xml)
-    end    
+    end
 
     it "should solrize correctly" do
-      ds = Dor::EmbargoMetadataDS.from_xml(@dsxml)
+      ds = Dor::EmbargoMetadataDS.from_xml(dsxml)
       ds.to_solr['embargo_release_date_dt'].should include('2011-10-12T22:47:52Z')
+      ds.to_solr['twenty_pct_visibility_release_date_dt'].should include('2016-10-12T22:47:52Z')
     end
   end
-  
+
   describe "#status" do
-    
+
     ds = Dor::EmbargoMetadataDS.new nil, 'embargoMetadata'
     ds.status = "released"
-    
+
     it "= sets status" do
       ds.term_values(:status).should == ["released"]
     end
-    
+
     it "= marks the datastream as dirty" do
       ds.should be_dirty
     end
-    
+
     it "gets the current value of status" do
       ds.status.should == "released"
     end
   end
-  
+
   describe "#release_date" do
-    
+
     ds = Dor::EmbargoMetadataDS.new nil, 'embargoMetadata'
     t = Time.now - 10
     ds.release_date = t
-    
+
     it "= sets releaseDate from a Time object as the start of day, UTC" do
       rd = Time.parse(ds.term_values(:release_date).first)
       rd.should == t.beginning_of_day.utc
     end
-    
+
     it "= marks the datastram as dirty" do
       ds.should be_dirty
     end
-    
+
     it "gets the current value of releaseDate as a Time object" do
       rd = ds.release_date
       rd.class.should == Time
@@ -93,7 +99,7 @@ describe Dor::EmbargoMetadataDS do
   end
 
   describe "releaseAccess manipulation" do
-    
+
     ds = Dor::EmbargoMetadataDS.new nil, 'embargoMetadata'
     nd = ds.release_access_node
 
@@ -118,22 +124,22 @@ describe Dor::EmbargoMetadataDS do
     		</access>
     	</embargoAccess>
       EOXML
-      
+
       ds.release_access_node = Nokogiri::XML(embargo_xml)
       embargo = ds.find_by_terms(:release_access)
       embargo.at_xpath("//releaseAccess/access[@type='read']/machine/world").should be
       ds.should be_dirty
     end
-    
+
     it "rejects Documents that do not have a root node of releaseAccess" do
       embargo_xml = "<incorrect/>"
       lambda { ds.release_access_node = Nokogiri::XML(embargo_xml) }.should raise_error
     end
   end
-  
+
   describe "Solr indexing" do
-    
-    
-    
+
+
+
   end
 end
