29130960d1f8d3af0b7683d71cfe1793bf0cfdac
Chris Beer <cabeer@stanford.edu>
2013-10-11 21:20:27 -0700

    remove utc date mapper

--- lib/dor-services.rb
+++ lib/dor-services.rb
@@ -72,7 +72,6 @@ module Dor
   # patches, utilities and helpers
   require 'dor/utils/ng_tidy'
   require 'dor/utils/solr_doc_helper'
-  require 'dor/utils/utc_date_field_mapper'
   require 'dor/utils/predicate_patch'
     
   require 'dor/datastreams/datastream_spec_solrizer'
