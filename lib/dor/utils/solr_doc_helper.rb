dc22fa8b344273abab58a1aa358447e9a6816dae
Chris Beer <cabeer@stanford.edu>
2013-10-11 21:28:54 -0700

    use the new solrizer 3.x insert field API

--- lib/dor/utils/solr_doc_helper.rb
+++ lib/dor/utils/solr_doc_helper.rb
@@ -1,9 +1,17 @@
 module SolrDocHelper
   
   def add_solr_value(solr_doc, field_name, value, field_type = :default, index_types = [:searchable])
-    index_types.each { |index_type|
-      ::Solrizer::Extractor.insert_solr_field_value(solr_doc, ::ActiveFedora::SolrService.solr_name(field_name, field_type, index_type), value)
-    }
+    if Solrizer::VERSION > "3"
+      case field_type
+        when :symbol
+          index_types << field_type
+      end
+      ::Solrizer.insert_field(solr_doc, field_name, value, *index_types)
+    else
+      index_types.each { |index_type|
+        ::Solrizer::Extractor.insert_solr_field_value(solr_doc, ::ActiveFedora::SolrService.solr_name(field_name, field_type, index_type), value)
+      }
+    end
   end
   
 end
