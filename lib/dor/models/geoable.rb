6a1cd826585c2091ab04a2137303682c7a39257c
Darren Hardy <drh@stanford.edu>
2014-01-27 13:44:55 -0800

    Added support for Geoable and GeoMetadataDS

---
+++ lib/dor/models/geoable.rb
@@ -0,0 +1,37 @@
+module Dor
+  module Geoable
+    extend ActiveSupport::Concern
+    include SolrDocHelper
+
+    class CrosswalkError < Exception; end    
+    
+    included do
+      has_metadata  :name => 'geoMetadata', 
+                    :type => Dor::GeoMetadataDS, 
+                    :label => 'Geographic Information Metadata in ISO 19139', 
+                    :format => 'gmd',
+                    :control_group => 'M'
+    end
+    
+    # @return [String] XML
+    def fetch_geoMetadata_datastream
+      candidates = self.datastreams['identityMetadata'].otherId.collect { |oid| oid.to_s }
+      metadata_id = Dor::MetadataService.resolvable(candidates).first
+      unless metadata_id.nil?
+        return Dor::MetadataService.fetch(metadata_id.to_s)
+      else
+        return nil
+      end
+    end
+
+    def build_geoMetadata_datastream(ds)
+      content = fetch_geoMetadata_datastream
+      unless content.nil?
+        ds.dsLabel = self.label
+        ds.ng_xml = Nokogiri::XML(content)
+        ds.ng_xml.normalize_text!
+        ds.content = ds.ng_xml.to_xml
+      end
+    end
+  end
+end
