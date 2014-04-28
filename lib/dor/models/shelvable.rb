4a213762cf23f6dcd0d644dfe22a450368ab5e95
Richard Anderson <rnanders@stanford.edu>
2014-04-28 14:44:36 -0600

    Revised shelve robot code to make unit tests more comprehensive

--- lib/dor/models/shelvable.rb
+++ lib/dor/models/shelvable.rb
@@ -7,18 +7,39 @@ module Dor
 
     # Push file changes for shelve-able files into the stacks
     def shelve
-      workspace_druid = DruidTools::Druid.new(id,Config.stacks.local_workspace_root)
-
+      # retrieve the differences between the current contentMetadata and the previously ingested version
+      shelve_diff = get_shelve_diff
+      # determine the location of the object's files in the stacks area
       stacks_druid = DruidTools::StacksDruid.new id, Config.stacks.local_stacks_root
       stacks_object_pathname = Pathname(stacks_druid.path)
+      # determine the location of the object's content files in the workspace area
+      workspace_druid = DruidTools::Druid.new(id,Config.stacks.local_workspace_root)
+      workspace_content_pathname = workspace_content_dir(shelve_diff, workspace_druid)
+      # delete, rename, or copy files to the stacks area
+      DigitalStacksService.remove_from_stacks(stacks_object_pathname, shelve_diff)
+      DigitalStacksService.rename_in_stacks(stacks_object_pathname, shelve_diff)
+      DigitalStacksService.shelve_to_stacks(workspace_content_pathname, stacks_object_pathname, shelve_diff)
+    end
 
+    # retrieve the differences between the current contentMetadata and the previously ingested version
+    # (filtering to select only the files that should be shelved to stacks)
+    def get_shelve_diff
       inventory_diff_xml = self.get_content_diff(:shelve)
       inventory_diff = Moab::FileInventoryDifference.parse(inventory_diff_xml)
-      content_diff = inventory_diff.group_difference("content")
+      shelve_diff = inventory_diff.group_difference("content")
+      shelve_diff
+    end
 
-      DigitalStacksService.remove_from_stacks(stacks_object_pathname, content_diff)
-      DigitalStacksService.rename_in_stacks(stacks_object_pathname, content_diff)
-      DigitalStacksService.shelve_to_stacks(workspace_druid, stacks_object_pathname, content_diff)
+    # Find the location of the object's content files in the workspace area
+    # @param [Moab::FileGroupDifference] content_diff The differences between the current contentMetadata and the previously ingested version
+    # @param [DruidTools::Druid] workspace_druid the location of the object's files in the workspace area
+    # @return [Pathname] The location of the object's content files in the workspace area
+    def workspace_content_dir (content_diff, workspace_druid)
+      deltas = content_diff.file_deltas
+      filelist = deltas[:modified] + deltas[:added] + deltas[:copyadded].collect{|old,new| new}
+      return nil if filelist.empty?
+      content_pathname = Pathname(workspace_druid.find_filelist_parent('content', filelist))
+      content_pathname
     end
 
   end
--- lib/dor/services/digital_stacks_service.rb
+++ lib/dor/services/digital_stacks_service.rb
@@ -5,27 +5,33 @@ module Dor
 
   class DigitalStacksService
 
-    def self.transfer_to_document_store(id, content, filename)
-      druid = DruidTools::PurlDruid.new id, Config.stacks.local_document_cache_root
-      druid.content_dir # create the druid tree if it doesn't exist yet
-      File.open(File.join(druid.content_dir, filename), 'w') { |f| f.write content }
-    end
-
     # Delete files from stacks that have change type 'deleted', 'copydeleted', or 'modified'
     # @param [Pathname] stacks_object_pathname the stacks location of the digital object
     # @param [Moab::FileGroupDifference] content_diff the content file version differences report
     def self.remove_from_stacks(stacks_object_pathname, content_diff)
       [:deleted, :copydeleted, :modified].each do |change_type|
-        subset = content_diff.subset(change_type) # {Moab::FileGroupDifferenceSubset
+        subset = content_diff.subset(change_type) # {Moab::FileGroupDifferenceSubset}
         subset.files.each do |moab_file| # {Moab::FileInstanceDifference}
           moab_signature = moab_file.signatures.first # {Moab::FileSignature}
           file_pathname = stacks_object_pathname.join(moab_file.basis_path)
-          if file_pathname.exist? and (file_pathname.size == moab_signature.size)
-            file_signature = Moab::FileSignature.new.signature_from_file(file_pathname)
-            file_pathname.delete if (file_signature == moab_signature)
-          end
+          self.delete_file(file_pathname, moab_signature)
         end
       end
+    end
+
+    # Delete a file, but only if it exists and matches the expected signature
+    # @param [Pathname] file_pathname The location of the file to be deleted
+    # @param [Moab::FileSignature] moab_signature The fixity values of the file
+    # @return [Boolean] true if file deleted, false otherwise
+    def self.delete_file(file_pathname, moab_signature)
+      if file_pathname.exist? and (file_pathname.size == moab_signature.size)
+        file_signature = Moab::FileSignature.new.signature_from_file(file_pathname)
+        if (file_signature == moab_signature)
+          file_pathname.delete
+          return true
+        end
+      end
+      return false
     end
 
     # Rename files from stacks that have change type 'renamed' using an intermediate temp filename.
@@ -39,63 +45,97 @@ module Dor
       subset.files.each do |moab_file| # {Moab::FileInstanceDifference}
         moab_signature = moab_file.signatures.first # {Moab::FileSignature}
         original_pathname = stacks_object_pathname.join(moab_file.basis_path)
-        if original_pathname.exist? and (original_pathname.size == moab_signature.size)
-          file_signature = Moab::FileSignature.new.signature_from_file(original_pathname)
-          if (file_signature == moab_signature)
-            temp_pathname = stacks_object_pathname.join(moab_signature.checksums.values.last)
-            original_pathname.rename(temp_pathname)
-          end
-        end
+        temp_pathname = stacks_object_pathname.join(moab_signature.checksums.values.last)
+        self.rename_file(original_pathname, temp_pathname, moab_signature)
       end
 
       # 2nd Pass - rename files from checksum-based name to new name
       subset.files.each do |moab_file| # {Moab::FileInstanceDifference}
         moab_signature = moab_file.signatures.first # {Moab::FileSignature}
         temp_pathname = stacks_object_pathname.join(moab_signature.checksums.values.last)
-        if temp_pathname.exist?
-          new_pathname = stacks_object_pathname.join(moab_file.other_path)
-          new_pathname.parent.mkpath
-          temp_pathname.rename(new_pathname)
-        end
+        new_pathname = stacks_object_pathname.join(moab_file.other_path)
+        self.rename_file(temp_pathname, new_pathname, moab_signature)
       end
 
     end
 
+    # Rename a file, but only if it exists and has the expected signature
+    # @param [Pathname] old_pathname The original location/name of the file being renamed
+    # @param [Pathname] new_pathname The new location/name of the file
+    # @param [Moab::FileSignature] moab_signature The fixity values of the file
+    # @return [Boolean] true if file renamed, false otherwise
+    def self.rename_file(old_pathname, new_pathname, moab_signature)
+      if old_pathname.exist? and (old_pathname.size == moab_signature.size)
+        file_signature = Moab::FileSignature.new.signature_from_file(old_pathname)
+        if (file_signature == moab_signature)
+          new_pathname.parent.mkpath
+          old_pathname.rename(new_pathname)
+          return true
+        end
+      end
+      return false
+    end
+
     # Add files to stacks that have change type 'added', 'copyadded' or 'modified'.
-    # @param [DruidTools::Druid] workspace_druid he dor workspace location of the digital object
-    # @param [Pathname] stacks_object_pathname the stacks location of the digital object
+    # @param [Pathname] workspace_content_pathname The dor workspace location of the digital object's content fies
+    # @param [Pathname] stacks_object_pathname the stacks location of the digital object's shelved files
     # @param [Moab::FileGroupDifference] content_diff the content file version differences report
-    def self.shelve_to_stacks(workspace_druid, stacks_object_pathname, content_diff)
+    def self.shelve_to_stacks(workspace_content_pathname, stacks_object_pathname, content_diff)
+      return false if workspace_content_pathname.nil?
       [:added, :copyadded, :modified,].each do |change_type|
         subset = content_diff.subset(change_type) # {Moab::FileGroupDifferenceSubset
         subset.files.each do |moab_file| # {Moab::FileInstanceDifference}
           moab_signature = moab_file.signatures.last # {Moab::FileSignature}
-          filename = (change_type == :modified) ? moab_file.base_path : moab_file.other_path
-          new_pathname = stacks_object_pathname.join(filename)
-          if new_pathname.exist?
-            file_signature = Moab::FileSignature.new.signature_from_file(new_pathname)
-            new_pathname.delete if (file_signature != moab_signature)
-          end
-          unless new_pathname.exist?
-            workspace_pathname = Pathname(workspace_druid.find_content(filename))
-            FileUtils.cp workspace_pathname.to_s, stacks_object_pathname.to_s
-          end
+          filename = (change_type == :modified) ? moab_file.basis_path : moab_file.other_path
+          workspace_pathname = workspace_content_pathname.join(filename)
+          stacks_pathname = stacks_object_pathname.join(filename)
+          self.copy_file(workspace_pathname, stacks_pathname, moab_signature)
         end
       end
-
-      # Assumes the digital stacks storage root is mounted to the local file system
-      # TODO since this is delegating to the Druid, this method may not be necessary
-      def self.prune_stacks_dir(id)
-        stacks_druid_tree = DruidTools::StacksDruid.new(id, Config.stacks.local_stacks_root)
-        stacks_druid_tree.prune!
-      end
-
-      def self.prune_purl_dir(id)
-        druid = DruidTools::PurlDruid.new(id, Dor::Config.stacks.local_document_cache_root)
-        druid.prune!
-      end
+      true
     end
 
+    # Copy a file to stacks, but only if it does not yet exist with the expected signature
+    # @param [Pathname] workspace_pathname The location of the file in the DOR workspace
+    # @param [Pathname] stacks_pathname The location of the file in the stacks
+    # @param [Moab::FileSignature] moab_signature The fixity values of the file
+    # @return [Boolean] true if file copied, false otherwise
+    def self.copy_file(workspace_pathname, stacks_pathname, moab_signature)
+      if stacks_pathname.exist?
+        file_signature = Moab::FileSignature.new.signature_from_file(stacks_pathname)
+        stacks_pathname.delete if (file_signature != moab_signature)
+      end
+      unless stacks_pathname.exist?
+        FileUtils.cp workspace_pathname.to_s, stacks_pathname.to_s
+        return true
+      end
+      return false
+    end
+
+    ### depricated ???
+
+    # Create a file inside the content directory under the stacks.local_document_cache_root
+    # @param [String] id The druid identifier for the object
+    # @param [String] content The contents of the file to be created
+    # @param [String] filename The name of the file to be created
+    # @return [void]
+    def self.transfer_to_document_store(id, content, filename)
+      druid = DruidTools::PurlDruid.new id, Config.stacks.local_document_cache_root
+      druid.content_dir # create the druid tree if it doesn't exist yet
+      File.open(File.join(druid.content_dir, filename), 'w') { |f| f.write content }
+    end
+
+    # Assumes the digital stacks storage root is mounted to the local file system
+    # TODO since this is delegating to the Druid, this method may not be necessary
+    def self.prune_stacks_dir(id)
+      stacks_druid_tree = DruidTools::StacksDruid.new(id, Config.stacks.local_stacks_root)
+      stacks_druid_tree.prune!
+    end
+
+    def self.prune_purl_dir(id)
+      druid = DruidTools::PurlDruid.new(id, Dor::Config.stacks.local_document_cache_root)
+      druid.prune!
+    end
   end
 
 end
