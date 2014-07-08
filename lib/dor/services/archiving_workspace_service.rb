431348400fc5f3eed1f6b1085f49f8682aff503b
Ahmed AlSum <aalsum@stanford.edu>
2014-07-08 12:47:24 -0700

    Update the archiving_workspace_service to use the version number

--- lib/dor/services/archiving_workspace_service.rb
+++ lib/dor/services/archiving_workspace_service.rb
@@ -3,9 +3,16 @@ module Dor
   # Rename the druid trees  at the end of the accessionWF in order to be cleaned/deleted later.
   class ArchivingWorkspaceService
 
-    def self.archive_workspace_druid_tree(druid, workspace_root)
+    def self.archive_workspace_druid_tree(druid, version, workspace_root)
+      
       druid_tree_path = DruidTools::Druid.new(druid, workspace_root).pathname.to_s
-      FileUtils.mv(druid_tree_path, druid_tree_path+"_v2")
+      
+      raise "The archived directory #{druid_tree_path}_v#{version} already existed." if  File.exists?("#{druid_tree_path}_v#{version}") 
+      
+      if File.exists?(druid_tree_path) 
+        FileUtils.mv(druid_tree_path, "#{druid_tree_path}_v#{version}")
+      end #Else is a truncated tree where we shouldn't do anything
+
     end
     
   end
--- spec/dor/archiving_workspace_service_spec.rb
+++ spec/dor/archiving_workspace_service_spec.rb
@@ -13,26 +13,50 @@ describe Dor::ArchivingWorkspaceService do
     before(:each) do
        @druid = "druid:am111am1111"
        @druid_tree_path = "#{@workspace_root}/am/111/am/1111/am111am1111"
-       
+      
+       @archived_druid = "druid:vr111vr1111"
+       @archived_druid_tree_path = "#{@workspace_root}/vr/111/vr/1111/vr111vr1111"
+
        #To make sure the directory name is as expected am111am1111
        FileUtils.mv(@druid_tree_path+"_v2", @druid_tree_path) if File.exists?(@druid_tree_path+"_v2")
     end
     
     it "should rename the directory tree with the directory not empty" do
-       Dor::ArchivingWorkspaceService.archive_workspace_druid_tree(@druid,@workspace_root)
+       Dor::ArchivingWorkspaceService.archive_workspace_druid_tree(@druid,"2",@workspace_root)
 
        File.exists?("#{@druid_tree_path}_v2").should eq true
        File.exists?(@druid_tree_path).should eq false
     end
+       
+    it "should do nothing with truncated druid" do
+      truncated_druid = "druid:tr111tr1111"
+      Dor::ArchivingWorkspaceService.archive_workspace_druid_tree(truncated_druid,"2",@workspace_root) 
+      truncated_druid_tree_path = "#{@workspace_root}/tr/111/tr/1111/"
+      
+      File.exists?("#{truncated_druid_tree_path}_v2").should eq false
+      File.exists?(truncated_druid_tree_path).should eq true
+    end
     
-    it "should throw an error if the directory doesn't exist" do
-       not_existent_druid = "druid:xx111xx1111"      
-       expect{ Dor::ArchivingWorkspaceService.archive_workspace_druid_tree(not_existent_druid,@workspace_root) }.to raise_error
+    it "should throw an error if the directory is already archived" do
+      expect{ Dor::ArchivingWorkspaceService.archive_workspace_druid_tree(@archived_druid,"2",@workspace_root) }.to raise_error
+    end
+    
+    it "should archived the current directory even if there is an older archived that hasn't been cleaned up" do
+      Dor::ArchivingWorkspaceService.archive_workspace_druid_tree(@archived_druid,"3",@workspace_root)
+ 
+      File.exists?("#{@archived_druid_tree_path}_v2").should eq true
+      File.exists?("#{@archived_druid_tree_path}_v3").should eq true
+      File.exists?("#{@archived_druid_tree_path}").should eq false
+      
+      #reset the env.
+
     end
     
     after(:each) do
       #To reset the environment to its original format
       FileUtils.mv(@druid_tree_path+"_v2", @druid_tree_path) if File.exists?(@druid_tree_path+"_v2")
+      FileUtils.mv( "#{@archived_druid_tree_path}_v3",@archived_druid_tree_path) if File.exists?(@archived_druid_tree_path+"_v3")
+
     end
     
   end
