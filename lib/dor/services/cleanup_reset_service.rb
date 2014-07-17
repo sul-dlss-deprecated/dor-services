769caab9c3512bcb174f1eea30d65e888ed96079
Ahmed AlSum <aalsum@stanford.edu>
2014-07-17 12:20:54 -0700

    Change the word renamed to reset

--- lib/dor/services/cleanup_reset_service.rb
+++ lib/dor/services/cleanup_reset_service.rb
@@ -5,13 +5,13 @@ module Dor
   # Remove all traces of the object's data files from the workspace and export areas
   class CleanupResetService
     
-    # @param [String] druid The identifier for the object whose renamed data is to be removed
-    # @return [void] remove copy of the renamed data that was exported to preservation core
-    def self.cleanup_by_renamed_druid(druid)
+    # @param [String] druid The identifier for the object whose reset data is to be removed
+    # @return [void] remove copy of the reset data that was exported to preservation core
+    def self.cleanup_by_reset_druid(druid)
       last_version = get_druid_last_version(druid) 
-      cleanup_renamed_workspace_content(druid, last_version, Config.cleanup.local_workspace_root)
-      cleanup_renamed_workspace_content(druid, last_version, Config.cleanup.local_assembly_root)
-      cleanup_renamed_export(druid, last_version)
+      cleanup_reset_workspace_content(druid, last_version, Config.cleanup.local_workspace_root)
+      cleanup_reset_workspace_content(druid, last_version, Config.cleanup.local_assembly_root)
+      cleanup_reset_export(druid, last_version)
     end
     
     def self.get_druid_last_version(druid)
@@ -24,49 +24,49 @@ module Dor
       end
     end
     
-    # @param [String] druid The identifier for the object whose renamed data is to be removed
+    # @param [String] druid The identifier for the object whose reset data is to be removed
     # @param [String] base The base directory to delete from
     # @param [Integer] last_version The last version that the data should be removed until version 1
-    # @return [void] remove all the object's renamed data files from the workspace area equal to less than the last_version
-    def self.cleanup_renamed_workspace_content(druid,last_version, base)
+    # @return [void] remove all the object's reset data files from the workspace area equal to less than the last_version
+    def self.cleanup_reset_workspace_content(druid,last_version, base)
       base_druid = DruidTools::Druid.new(druid, base)
       base_druid_tree = base_druid.pathname.to_s
       #if it is truncated tree /aa/111/aaa/1111/content, 
       #we should follow the regular cleanup technique
 
-      renamed_directories = get_renamed_dir_list(last_version, base_druid_tree)
-      renamed_directories.each do |path| 
+      reset_directories = get_reset_dir_list(last_version, base_druid_tree)
+      reset_directories.each do |path| 
         FileUtils.rm_rf(path)
       end
       base_druid.prune_ancestors(base_druid.pathname.parent)
     end
     
     
-    # @param [String] druid The identifier for the object whose renamed data is to be removed
+    # @param [String] druid The identifier for the object whose reset data is to be removed
     # @param [String] base The base directory to delete from
     # @param [Integer] last_version The last version that the data should be removed until version 1
-    # @return [void] prepares a list of renamed directories that should be removed
-    def self.get_renamed_dir_list(last_version, base_druid_tree)
-      renamed_directories = []    
+    # @return [void] prepares a list of reset directories that should be removed
+    def self.get_reset_dir_list(last_version, base_druid_tree)
+      reset_directories = []    
       for i in 1..last_version 
-        renamed_path = "#{base_druid_tree}_v#{i}"
-        renamed_directories.append(renamed_path) if File.exists?(renamed_path) 
+        reset_path = "#{base_druid_tree}_v#{i}"
+        reset_directories.append(reset_path) if File.exists?(reset_path) 
       end
-      return renamed_directories      
+      return reset_directories      
     end
 
-    # @param [String] druid The identifier for the object whose renamed bags data is to be removed
-    # @return [void] remove copy of the renamed data that was exported to preservation core
-    def self.cleanup_renamed_export(druid, last_version)
+    # @param [String] druid The identifier for the object whose reset bags data is to be removed
+    # @return [void] remove copy of the reset data that was exported to preservation core
+    def self.cleanup_reset_export(druid, last_version)
       id = druid.split(':').last
       base_bag_directory = File.join(Config.cleanup.local_export_home, id)
       
-      bag_dir_list = get_renamed_bag_dir_list(last_version, base_bag_directory)
+      bag_dir_list = get_reset_bag_dir_list(last_version, base_bag_directory)
       bag_dir_list.each do |bag_dir| 
         Pathname(bag_dir).rmtree
       end
       
-      bag_tar_list = get_renamed_bag_tar_list(last_version, base_bag_directory)
+      bag_tar_list = get_reset_bag_tar_list(last_version, base_bag_directory)
       bag_tar_list.each do |bag_tar| 
         Pathname(bag_tar).rmtree
       end
@@ -74,26 +74,26 @@ module Dor
     
     # @param [Integer] last_version The last version that the data should be removed until version 1
     # @param [String] base_bag_directory The base bag directory including the export home and druid id
-    # @return [void] prepares a list of renamed bag directories that should be removed
-    def self.get_renamed_bag_dir_list(last_version, base_bag_directory)
-      renamed_bags = []    
+    # @return [void] prepares a list of reset bag directories that should be removed
+    def self.get_reset_bag_dir_list(last_version, base_bag_directory)
+      reset_bags = []    
       for i in 1..last_version do
-        renamed_path = "#{base_bag_directory}_v#{i}"
-        renamed_bags.append(renamed_path) if File.exists?(renamed_path) 
+        reset_path = "#{base_bag_directory}_v#{i}"
+        reset_bags.append(reset_path) if File.exists?(reset_path) 
       end
-      return renamed_bags      
+      return reset_bags      
     end
 
     # @param [String] base_bag_directory The base bag directory including the export home and druid id
     # @param [Integer] last_version The last version that the data should be removed until version 1
-    # @return [void] prepares a list of renamed bag tars that should be removed
-    def self.get_renamed_bag_tar_list(last_version, base_bag_directory)
-      renamed_bags = []    
+    # @return [void] prepares a list of reset bag tars that should be removed
+    def self.get_reset_bag_tar_list(last_version, base_bag_directory)
+      reset_bags = []    
       for i in 1..last_version do
-        renamed_path = "#{base_bag_directory}_v#{i}.tar"
-        renamed_bags.append(renamed_path)  if File.exists?(renamed_path) 
+        reset_path = "#{base_bag_directory}_v#{i}.tar"
+        reset_bags.append(reset_path)  if File.exists?(reset_path) 
       end
-      return renamed_bags      
+      return reset_bags      
     end
 
   end
--- spec/dor/cleanup_reset_service_spec.rb
+++ spec/dor/cleanup_reset_service_spec.rb
@@ -21,16 +21,16 @@ describe Dor::CleanupResetService do
     @export_pathname.rmtree if @export_pathname.exist?
   end
 
-  # clean_renamed_workspace
+  # clean_reset_workspace
   
  # ck111ck 
     
- context "cleanup_by_renamed_druid" do
+ context "cleanup_by_reset_druid" do
    before(:each) do
      
    end
    
-  it "should remove the renamed druid tree from workspace and renamed bag from export" do
+  it "should remove the reset druid tree from workspace and reset bag from export" do
     druid_id = "cc111cm1111"
     druid = "druid:#{druid_id}"
     create_bag_dir(druid_id+"_v1")
@@ -46,7 +46,7 @@ describe Dor::CleanupResetService do
     expect(File.exists?(base_druid_dir+"_v1")).to eq true    
 
     Dor::CleanupResetService.stub(:get_druid_last_version).and_return(1)
-    Dor::CleanupResetService.cleanup_by_renamed_druid(druid)
+    Dor::CleanupResetService.cleanup_by_reset_druid(druid)
 
     expect(File.exists?(base_bag_dir)).to eq false
     expect(File.exists?(base_tar_dir)).to eq false
@@ -56,14 +56,14 @@ describe Dor::CleanupResetService do
  
  end
  
- #cleanup_renamed_workspace_content
+ #cleanup_reset_workspace_content
  ## cc111ci1111 - 1 version
  ## cc111cj1111 - 1 opened and 1 versioned
  ## cz111cz1111 - 1 version with root ancestor
  ## cc111ck1111 - 1 version with immediate ancestor
  ### cc111ck1112 support the previous version
-context "cleanup_renamed_workspace_content" do
-  it "should remove the renamed directory in workspace" do
+context "cleanup_reset_workspace_content" do
+  it "should remove the reset directory in workspace" do
     druid_id = "cc111ci1111"
     druid = "druid:#{druid_id}"
     create_workspace_dir(druid_id,1)
@@ -71,11 +71,11 @@ context "cleanup_renamed_workspace_content" do
     base_druid_dir = DruidTools::Druid.new(druid, @workspace_root_pathname.to_s).pathname.to_s
     last_version = 1
     expect(File.exists?(base_druid_dir+"_v1")).to eq true
-    Dor::CleanupResetService.cleanup_renamed_workspace_content(druid, last_version, @workspace_root_pathname.to_s)
+    Dor::CleanupResetService.cleanup_reset_workspace_content(druid, last_version, @workspace_root_pathname.to_s)
     expect(File.exists?(base_druid_dir+"_v1")).to eq false
   end
   
-  it "should remove the renamed directory and keep the open version" do
+  it "should remove the reset directory and keep the open version" do
     druid_id = "cc111cj1111"
     druid = "druid:#{druid_id}"
     create_workspace_dir(druid_id,2)
@@ -86,7 +86,7 @@ context "cleanup_renamed_workspace_content" do
     last_version = 2
     expect(File.exists?(base_druid_dir+"_v2")).to eq true
     expect(File.exists?(base_druid_dir+"_v3")).to eq true
-    Dor::CleanupResetService.cleanup_renamed_workspace_content(druid, last_version, @workspace_root_pathname.to_s)
+    Dor::CleanupResetService.cleanup_reset_workspace_content(druid, last_version, @workspace_root_pathname.to_s)
     expect(File.exists?(base_druid_dir+"_v2")).to eq false
     expect(File.exists?(base_druid_dir+"_v3")).to eq true
   end
@@ -99,7 +99,7 @@ context "cleanup_renamed_workspace_content" do
     
     last_version = 1
     expect(File.exists?(base_druid_dir+"_v1")).to eq true
-    Dor::CleanupResetService.cleanup_renamed_workspace_content(druid, last_version, @workspace_root_pathname.to_s)
+    Dor::CleanupResetService.cleanup_reset_workspace_content(druid, last_version, @workspace_root_pathname.to_s)
     expect(File.exists?(@workspace_root_pathname.join("cz"))).to eq false
   end
 
@@ -117,7 +117,7 @@ context "cleanup_renamed_workspace_content" do
     last_version = 1
     expect(File.exists?(base_druid_dir_1+"_v1")).to eq true
     expect(File.exists?(base_druid_dir_2)).to eq true
-    Dor::CleanupResetService.cleanup_renamed_workspace_content(druid_1, last_version, @workspace_root_pathname.to_s)
+    Dor::CleanupResetService.cleanup_reset_workspace_content(druid_1, last_version, @workspace_root_pathname.to_s)
     expect(File.exists?(base_druid_dir_1+"_v1")).to eq false
     expect(File.exists?(base_druid_dir_2)).to eq true
     expect(File.exists?(@workspace_root_pathname.join("cc").join("111").join("ck").join("1111"))).to eq false
@@ -131,7 +131,7 @@ end
   ## cc111cg1111 - 2 version (v2 and v3)
   ## cc111ch1111 - 1 version (v1) and 1 opened version (v2)
  
- context "get_renamed_dirctories_list" do
+ context "get_reset_dirctories_list" do
    before(:each) do
      @druid_1v = "cc111cf1111"
      @druid_2v = "cc111cg1111"
@@ -144,22 +144,22 @@ end
      create_workspace_dir(@druid_1_1v, 2)
    end
    
-  it "should get one renamed directory from workspace" do
+  it "should get one reset directory from workspace" do
     druid = "druid:#{@druid_1v}"
     base_druid_tree = DruidTools::Druid.new(druid, @workspace_root_pathname.to_s)
     last_version = 1
-    dir_list = Dor::CleanupResetService.get_renamed_dir_list(last_version, base_druid_tree.path)
+    dir_list = Dor::CleanupResetService.get_reset_dir_list(last_version, base_druid_tree.path)
     
      expect_dir_path = "#{base_druid_tree.path}_v1"
      expect(dir_list.length).to eq 1
      expect(dir_list[0]).to eq expect_dir_path
   end
 
-  it "should get two renamed directories from workspace" do
+  it "should get two reset directories from workspace" do
     druid = "druid:#{@druid_2v}"
     base_druid_tree = DruidTools::Druid.new(druid, @workspace_root_pathname.to_s)
     last_version = 3
-    dir_list = Dor::CleanupResetService.get_renamed_dir_list(last_version, base_druid_tree.path)
+    dir_list = Dor::CleanupResetService.get_reset_dir_list(last_version, base_druid_tree.path)
     
      expect_dir_path_1 = "#{base_druid_tree.path}_v2"
      expect_dir_path_2 = "#{base_druid_tree.path}_v3"
@@ -168,11 +168,11 @@ end
      expect(dir_list[1]).to eq expect_dir_path_2
   end   
 
-  it "should get one renamed directory from workspace and avoid the open version" do
+  it "should get one reset directory from workspace and avoid the open version" do
     druid = "druid:#{@druid_1_1v}"
     base_druid_tree = DruidTools::Druid.new(druid, @workspace_root_pathname.to_s)
     last_version = 1
-    dir_list = Dor::CleanupResetService.get_renamed_dir_list(last_version, base_druid_tree.path)
+    dir_list = Dor::CleanupResetService.get_reset_dir_list(last_version, base_druid_tree.path)
     
      expect_dir_path = "#{base_druid_tree.path}_v1"
      expect(dir_list.length).to eq 1
@@ -182,9 +182,9 @@ end
  
   # export
   ## cc111cd1111 - 1 dir and 1 tar
-  ## cc111ce1111 - 2 dir opened and versioned and 2 tar opened and renamed
+  ## cc111ce1111 - 2 dir opened and versioned and 2 tar opened and reset
   ## 
- context "cleanup_renamed_export" do
+ context "cleanup_reset_export" do
    before(:each) do
      @druid_1v = "cc111cd1111"
      create_bag_dir(@druid_1v+"_v1")
@@ -196,13 +196,13 @@ end
      base_bag_dir = "#{@export_pathname}/#{@druid_1v}_v1"
      expect(File.exists?(base_bag_dir)).to eq true
      expect(File.exists?(base_bag_dir+".tar")).to eq true
-     Dor::CleanupResetService.cleanup_renamed_export(druid,1)
+     Dor::CleanupResetService.cleanup_reset_export(druid,1)
      expect(File.exists?(base_bag_dir)).to eq false
      expect(File.exists?(base_bag_dir+".tar")).to eq false
    end
  end
  
- context "get_renamed_bag_dir_list" do
+ context "get_reset_bag_dir_list" do
    before(:each) do
      @druid_1v = "cc111ca1111"
      @druid_2v = "cc111cb1111"
@@ -218,7 +218,7 @@ end
    it "should read the bag directory with 1 version" do
      druid = "druid:#{@druid_1v}"
      base_bag_dir = "#{@export_pathname}/#{@druid_1v}"
-     dir_list =  Dor::CleanupResetService.get_renamed_bag_dir_list(1,base_bag_dir)
+     dir_list =  Dor::CleanupResetService.get_reset_bag_dir_list(1,base_bag_dir)
      
      expect_dir_file = "#{base_bag_dir}_v1"
      expect(dir_list.length).to eq 1
@@ -228,7 +228,7 @@ end
    it "should return a list of bag directories with 2 versions" do
      druid = "druid:#{@druid_2v}"
      base_bag_dir = "#{@export_pathname}/#{@druid_2v}"
-     dir_list =  Dor::CleanupResetService.get_renamed_bag_dir_list(3,base_bag_dir)
+     dir_list =  Dor::CleanupResetService.get_reset_bag_dir_list(3,base_bag_dir)
      
      expect_dir_file_1 = "#{base_bag_dir}_v2"
      expect_dir_file_2 = "#{base_bag_dir}_v3"
@@ -240,7 +240,7 @@ end
    it "should return a list of tars with 1 version and 1 opened version" do
      druid = "druid:#{@druid_1_1v}"
      base_bag_dir = "#{@export_pathname}/#{@druid_1_1v}"
-     dir_list =  Dor::CleanupResetService.get_renamed_bag_dir_list(1,base_bag_dir)
+     dir_list =  Dor::CleanupResetService.get_reset_bag_dir_list(1,base_bag_dir)
      
      expect_dir_file = "#{base_bag_dir}_v1"
      expect(dir_list.length).to eq 1
@@ -248,7 +248,7 @@ end
    end
  end
  
- context "get_renamed_bag_tar_list" do
+ context "get_reset_bag_tar_list" do
    before(:each) do
      @druid_1v = "cc111ca1111"
      @druid_2v = "cc111cb1111"
@@ -264,7 +264,7 @@ end
    it "should return a list of tars with 1 version" do
      druid = "druid:#{@druid_1v}"
      base_bag_dir = "#{@export_pathname}/#{@druid_1v}"
-     tar_list =  Dor::CleanupResetService.get_renamed_bag_tar_list(1,base_bag_dir)
+     tar_list =  Dor::CleanupResetService.get_reset_bag_tar_list(1,base_bag_dir)
      
      expect_tar_file = "#{base_bag_dir}_v1.tar"
      expect(tar_list.length).to eq 1
@@ -274,7 +274,7 @@ end
    it "should return a list of tars with 2 versions" do
      druid = "druid:#{@druid_2v}"
      base_bag_dir = "#{@export_pathname}/#{@druid_2v}"
-     tar_list =  Dor::CleanupResetService.get_renamed_bag_tar_list(3,base_bag_dir)
+     tar_list =  Dor::CleanupResetService.get_reset_bag_tar_list(3,base_bag_dir)
      
      expect_tar_file_1 = "#{base_bag_dir}_v2.tar"
      expect_tar_file_2 = "#{base_bag_dir}_v3.tar"
@@ -286,7 +286,7 @@ end
    it "should return a list of tars with 1 version and 1 opened version" do
      druid = "druid:#{@druid_1_1v}"
      base_bag_dir = "#{@export_pathname}/#{@druid_1_1v}"
-     tar_list =  Dor::CleanupResetService.get_renamed_bag_tar_list(1,base_bag_dir)
+     tar_list =  Dor::CleanupResetService.get_reset_bag_tar_list(1,base_bag_dir)
      
      expect_tar_file = "#{base_bag_dir}_v1.tar"
      expect(tar_list.length).to eq 1
