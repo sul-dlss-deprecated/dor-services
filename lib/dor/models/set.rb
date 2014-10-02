5068bddccc2dea0d6e448ebc1f05f50db7ef09d1
Johnathan Martin <john.martin@stanford.edu>
2014-10-02 16:43:05 -0700

    ARGO-346:  include Versionable so that the versionMetadata datastream has the correct type (Dor::VersionMetadataDS instead of ActiveFedora::Datastream)

--- lib/dor/models/set.rb
+++ lib/dor/models/set.rb
@@ -5,6 +5,7 @@ module Dor
     include Governable
     include Describable
     include Publishable
+    include Versionable
 
     has_many :members, :property => :is_member_of_collection, :inbound => true, :class_name => "ActiveFedora::Base"
     has_object_type 'set'
