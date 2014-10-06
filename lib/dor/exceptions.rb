e2507cbd9fa6f12075ba8956ba19a044ba8f1fbc
Willy Mene <wmene@stanford.edu>
2014-10-06 11:24:37 -0700

    Dor::Exception should subclass StandardError.  Fixes #13

--- lib/dor/exceptions.rb
+++ lib/dor/exceptions.rb
@@ -1,10 +1,10 @@
 module Dor
-  
-  class Exception < ::Exception; end
+
+  class Exception < ::StandardError; end
   class ParameterError < Exception; end
   class DuplicateIdError < Exception
     attr_reader :pid
-    
+
     def initialize(pid)
       @pid = pid
     end
