2e37c0321bb905f0559d2fdb6d2339e52dec2b81
Johnathan Martin <john.martin@stanford.edu>
2014-11-11 13:00:21 -0800

    improve formatting of xml in unit tests

--- spec/dor/document_spec.rb
+++ spec/dor/document_spec.rb
@@ -13,8 +13,11 @@ describe Dor::Workflow::Document do
   end
   describe 'processes' do
     it 'should generate an empty response from empty xml' do
-      xml='<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-      <workflow objectId="druid:mf777zb0743" id="sdrIngestWF"/>'
+      xml = <<-eos
+      <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+      <workflow objectId="druid:mf777zb0743" id="sdrIngestWF"/>
+      eos
+
       #xml=Nokogiri::XML(xml)
       d=Dor::Workflow::Document.new(xml)
       d.stub(:definition).and_return(@wf_definition)
@@ -22,13 +25,16 @@ describe Dor::Workflow::Document do
     end
 
     it 'should generate a process list based on the reified workflow which has sequence, not the processes list from the workflow service' do
-      xml='<?xml version="1.0" encoding="UTF-8"?>
+      xml = <<-eos
+      <?xml version="1.0" encoding="UTF-8"?>
       <workflow repository="dor" objectId="druid:gv054hp4128" id="accessionWF">
-      <process version="2" lifecycle="submitted" elapsed="0.0" archived="true" attempts="1"
-      datetime="2012-11-06T16:18:24-0800" status="completed" name="start-accession"/>
-      <process version="2" elapsed="0.0" archived="true" attempts="1"
-      datetime="2012-11-06T16:18:58-0800" status="completed" name="technical-metadata"/>
-      '
+        <process version="2" lifecycle="submitted" elapsed="0.0" archived="true" attempts="1"
+         datetime="2012-11-06T16:18:24-0800" status="completed" name="start-accession"/>
+        <process version="2" elapsed="0.0" archived="true" attempts="1"
+         datetime="2012-11-06T16:18:58-0800" status="completed" name="technical-metadata"/>
+      </workflow>
+      eos
+
       d=Dor::Workflow::Document.new(xml)
       d.stub(:definition).and_return(@wf_definition)
       d.processes.length.should == 3
@@ -42,25 +48,31 @@ describe Dor::Workflow::Document do
   end
   describe 'expedited?' do
     it 'says false if there are no prioritized items' do
-      xml='<?xml version="1.0" encoding="UTF-8"?>
+      xml = <<-eos
+      <?xml version="1.0" encoding="UTF-8"?>
       <workflow repository="dor" objectId="druid:gv054hp4128" id="accessionWF">
-      <process version="2" lifecycle="submitted" elapsed="0.0" archived="true" attempts="1"
-      datetime="2012-11-06T16:18:24-0800" status="completed" name="start-accession"/>
-      <process version="2" elapsed="0.0" archived="true" attempts="1"
-      datetime="2012-11-06T16:18:58-0800" status="completed" name="technical-metadata"/>
-      '
+        <process version="2" lifecycle="submitted" elapsed="0.0" archived="true" attempts="1"
+         datetime="2012-11-06T16:18:24-0800" status="completed" name="start-accession"/>
+        <process version="2" elapsed="0.0" archived="true" attempts="1"
+         datetime="2012-11-06T16:18:58-0800" status="completed" name="technical-metadata"/>
+      </workflow>
+      eos
+
       d=Dor::Workflow::Document.new(xml)
       d.stub(:definition).and_return(@wf_definition)
       d.expedited?.should == false
     end
     it 'says true if there are incomplete prioritized items' do
-      xml='<?xml version="1.0" encoding="UTF-8"?>
+      xml = <<-eos
+      <?xml version="1.0" encoding="UTF-8"?>
       <workflow repository="dor" objectId="druid:gv054hp4128" id="accessionWF">
-      <process version="2" lifecycle="submitted" elapsed="0.0" archived="true" attempts="1"
-      datetime="2012-11-06T16:18:24-0800" status="completed" name="start-accession"/>
-      <process version="2" elapsed="0.0" archived="true" attempts="1"
-      datetime="2012-11-06T16:18:58-0800" status="waiting" priority="50" name="technical-metadata"/>
-      '
+        <process version="2" lifecycle="submitted" elapsed="0.0" archived="true" attempts="1"
+         datetime="2012-11-06T16:18:24-0800" status="completed" name="start-accession"/>
+        <process version="2" elapsed="0.0" archived="true" attempts="1"
+         datetime="2012-11-06T16:18:58-0800" status="waiting" priority="50" name="technical-metadata"/>
+      </workflow>
+      eos
+
       d=Dor::Workflow::Document.new(xml)
       d.stub(:definition).and_return(@wf_definition)
       d.expedited?.should == true
@@ -68,25 +80,31 @@ describe Dor::Workflow::Document do
   end 
   describe 'active?' do
     it 'should return true if there are any non-archived rows' do
-      xml='<?xml version="1.0" encoding="UTF-8"?>
+      xml = <<-eos
+      <?xml version="1.0" encoding="UTF-8"?>
       <workflow repository="dor" objectId="druid:gv054hp4128" id="accessionWF">
-      <process lifecycle="submitted" elapsed="0.0" archived="true" attempts="1"
-      datetime="2012-11-06T16:18:24-0800" status="completed" name="start-accession"/>
-      <process version="2" elapsed="0.0" archived="true" attempts="1"
-      datetime="2012-11-06T16:18:58-0800" status="waiting" priority="50" name="technical-metadata"/>
-      '
+        <process lifecycle="submitted" elapsed="0.0" archived="true" attempts="1"
+         datetime="2012-11-06T16:18:24-0800" status="completed" name="start-accession"/>
+        <process version="2" elapsed="0.0" archived="true" attempts="1"
+         datetime="2012-11-06T16:18:58-0800" status="waiting" priority="50" name="technical-metadata"/>
+      </workflow>
+      eos
+
       d=Dor::Workflow::Document.new(xml)
       d.stub(:definition).and_return(@wf_definition)
       d.expedited?.should == true
     end
     it 'should return false if there are only archived rows' do
-      xml='<?xml version="1.0" encoding="UTF-8"?>
+      xml = <<-eos
+      <?xml version="1.0" encoding="UTF-8"?>
       <workflow repository="dor" objectId="druid:gv054hp4128" id="accessionWF">
-      <process version="2" lifecycle="submitted" elapsed="0.0" archived="true" attempts="1"
-      datetime="2012-11-06T16:18:24-0800" status="completed" name="start-accession"/>
-      <process version="2" elapsed="0.0" archived="true" attempts="1"
-      datetime="2012-11-06T16:18:58-0800" status="waiting" priority="50" name="technical-metadata"/>
-      '
+        <process version="2" lifecycle="submitted" elapsed="0.0" archived="true" attempts="1"
+         datetime="2012-11-06T16:18:24-0800" status="completed" name="start-accession"/>
+        <process version="2" elapsed="0.0" archived="true" attempts="1"
+         datetime="2012-11-06T16:18:58-0800" status="waiting" priority="50" name="technical-metadata"/>
+      </workflow>
+      eos
+
       d=Dor::Workflow::Document.new(xml)
       d.stub(:definition).and_return(@wf_definition)
       d.expedited?.should == true
@@ -94,13 +112,16 @@ describe Dor::Workflow::Document do
   end
   describe 'to_solr' do
     it 'should create the workflow_status field with the workflow repository included' do
-      xml='<?xml version="1.0" encoding="UTF-8"?>
+      xml = <<-eos
+      <?xml version="1.0" encoding="UTF-8"?>
       <workflow repository="dor" objectId="druid:gv054hp4128" id="accessionWF">
-      <process version="2" lifecycle="submitted" elapsed="0.0" archived="true" attempts="1"
-      datetime="2012-11-06T16:18:24-0800" status="completed" name="start-accession"/>
-      <process version="2" elapsed="0.0" archived="true" attempts="1"
-      datetime="2012-11-06T16:18:58-0800" status="completed" name="technical-metadata"/>
-      '
+        <process version="2" lifecycle="submitted" elapsed="0.0" archived="true" attempts="1"
+         datetime="2012-11-06T16:18:24-0800" status="completed" name="start-accession"/>
+        <process version="2" elapsed="0.0" archived="true" attempts="1"
+         datetime="2012-11-06T16:18:58-0800" status="completed" name="technical-metadata"/>
+      </workflow>
+      eos
+
       d=Dor::Workflow::Document.new(xml)   
       d.stub(:definition).and_return(@wf_definition)  
       doc=d.to_solr
@@ -150,12 +171,16 @@ describe Dor::Workflow::Document do
     end
     
     it 'should index error messages' do
-      xml='<?xml version="1.0" encoding="UTF-8"?>
+      xml = <<-eos
+      <?xml version="1.0" encoding="UTF-8"?>
       <workflow repository="dor" objectId="druid:gv054hp4128" id="accessionWF">
-      <process version="2" lifecycle="submitted" elapsed="0.0" archived="true" attempts="1"
-      datetime="2012-11-06T16:18:24-0800" status="completed" name="start-accession"/>
-      <process version="2" elapsed="0.0" archived="true" attempts="1"
-      datetime="2012-11-06T16:18:58-0800" status="error" errorMessage="druid:gv054hp4128 - Item error; caused by 413 Request Entity Too Large:" name="technical-metadata"/>'
+        <process version="2" lifecycle="submitted" elapsed="0.0" archived="true" attempts="1"
+         datetime="2012-11-06T16:18:24-0800" status="completed" name="start-accession"/>
+        <process version="2" elapsed="0.0" archived="true" attempts="1"
+         datetime="2012-11-06T16:18:58-0800" status="error" errorMessage="druid:gv054hp4128 - Item error; caused by 413 Request Entity Too Large:" name="technical-metadata"/>
+      </workflow>
+      eos
+
       d=Dor::Workflow::Document.new(xml)   
       d.stub(:definition).and_return(@wf_definition)  
       doc=d.to_solr
