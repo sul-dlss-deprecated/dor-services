0c6ae6f0b2ebfc452054eb2a9ae16a3fc8abb9db
Joe Atzberger <ohiocore@gmail.com>
2015-01-13 13:15:51 -0800

    Whitespace for readability

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -31,16 +31,16 @@ module Dor::SpecHelpers
     Dor::Config.push! do
       suri.mint_ids false
       gsearch do
-        url "http://solr.edu/gsearch"
-        rest_url "http://fedora.edu/gsearch/rest"
+        url        "http://solr.edu/gsearch"
+        rest_url   "http://fedora.edu/gsearch/rest"
       end
       solrizer.url "http://solr.edu/solrizer"
-      fedora.url "http://fedora.edu/fedora"
-      stacks.local_workspace_root File.join(fixture_dir, "workspace")
-      stacks.local_stacks_root File.join(fixture_dir, "stacks")
+      fedora.url   "http://fedora.edu/fedora"
+      stacks.local_workspace_root      File.join(fixture_dir, "workspace")
+      stacks.local_stacks_root         File.join(fixture_dir, "stacks")
       stacks.local_document_cache_root File.join(fixture_dir, "purl")
-      sdr.local_workspace_root File.join(fixture_dir, "workspace")
-      sdr.local_export_home File.join(fixture_dir, "export")
+      sdr.local_workspace_root         File.join(fixture_dir, "workspace")
+      sdr.local_export_home            File.join(fixture_dir, "export")
     end
     allow(ActiveFedora).to receive(:fedora).and_return(double('frepo').as_null_object)
   end
