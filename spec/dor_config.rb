49b616187c7cdd8a5ef0f05bb4b219a44975718f
Willy Mene <wmene@stanford.edu>
2015-01-22 12:00:26 -0800

    Add spec/dor_config for testing config placeholders

---
+++ spec/dor_config.rb
@@ -0,0 +1,33 @@
+cert_dir = File.join(File.dirname(__FILE__),"certs")
+
+Dor.configure do
+  ssl do
+    cert_file File.join(cert_dir,"spec.crt")
+    key_file File.join(cert_dir,"spec.key")
+    key_pass ''
+  end
+
+  fedora.url 'http://fedoraUser:fedoraPass@example.edu/fedora'
+
+  suri do
+    mint_ids true
+    id_namespace 'druid'
+    url 'http://example.edu/suri'
+    user 'hydra-etd'
+    pass 'lyberteam'
+  end
+
+  metadata do
+    exist.url 'http://mdtkUser:mdtkPass@example.edu/exist/rest/'
+    catalog.url 'http://example.edu/catalog/mods'
+  end
+
+  gsearch do
+    rest_url 'https://example.edu/gsearch/rest'
+    url 'https://example.edu/solr/gsearch'
+  end
+  solrizer.url 'https://example.edu/solr/solrizer'
+  workflow.url 'http://example.edu/workflow/'
+  sdr.url 'http://example.edu/sdr'
+  dor_services.url 'https://example.edu/dor'
+end
