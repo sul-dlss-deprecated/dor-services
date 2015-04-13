1576d2d944290fda5a250e0fb29d82967526a326
Joe Atzberger <ohiocore@gmail.com>
2015-04-13 14:21:28 -0700

    dor-rights-auth unfinished scaffolding

--- lib/dor/datastreams/rights_metadata_ds.rb
+++ lib/dor/datastreams/rights_metadata_ds.rb
@@ -1,66 +1,57 @@
 module Dor
-class RightsMetadataDS < ActiveFedora::OmDatastream
-  include SolrDocHelper
-  require 'dor/rights_auth'
+  class RightsMetadataDS < ActiveFedora::OmDatastream
+    require 'dor/rights_auth'
 
-  # This is separate from default_object_rights because
-  # (1) we cannot default to such a permissive state
-  # (2) this is real, not default
+    # This is separate from default_object_rights because
+    # (1) we cannot default to such a permissive state
+    # (2) this is real, not default
 
-  def self.xml_template
-    Nokogiri::XML::Builder.new do |xml|
-      xml.rightsMetadata{
-        xml.access(:type => 'discover'){
-          xml.machine{ xml.none }
-        }
-        xml.access(:type => 'read'){
-          xml.machine{ xml.none }
-        }
-        xml.use{
-          xml.human(:type => 'useAndReproduction')
-          xml.human(:type => "creativeCommons")
-          xml.machine(:type => "creativeCommons")
-        }
-        xml.copyright{ xml.human }
-      }
-    end.doc
-  end
+    set_terminology do |t|
+    #  t.root :path => 'rightsMetadata', :index_as => [:not_searchable]
+      t.root :path => 'rightz', :index_as => [:not_searchable]
+      t.copyright :path => 'copyright/human', :index_as => [:facetable]
+      t.use_statement :path => '/use/human[@type=\'useAndReproduction\']', :index_as => [:facetable]
 
-  set_terminology do |t|
-    t.root          :path => 'rightsMetadata',                           :index_as => [:not_searchable]
-    t.copyright     :path => '/copyright/human',                         :index_as => [:facetable]
-    t.use_statement :path => '/use/human[@type=\'useAndReproduction\']', :index_as => [:facetable]
-
-    t.use do
-      t.machine
-      t.human
-    end
-
-    t.creative_commons       :path => '/use/machine', :type => 'creativeCommons'
-    t.creative_commons_human :path => '/use/human[@type=\'creativeCommons\']'
-  end
-
-  define_template :creative_commons do |xml|
-    xml.use {
-      xml.human(:type => "creativeCommons")
-      xml.machine(:type => "creativeCommons")
-    }
-  end
-
-  def to_solr(solr_doc=Hash.new, *args)
-    super(solr_doc, *args)
-    if digital_object.respond_to?(:profile)
-      digital_object.profile.each_pair do |property,value|
-        add_solr_value(solr_doc, property.underscore, value, property =~ /Date/ ? :date : :string, [:searchable])
+      t.use do
+        t.machine
+        t.human
       end
+
+      t.creative_commons :path => '/use/machine', :type => 'creativeCommons'
+      t.creative_commons_human :path => '/use/human[@type=\'creativeCommons\']'
     end
-    if sourceId.present?
-      (name,id) = sourceId.split(/:/,2)
-      add_solr_value(solr_doc, "dor_id", id, :string, [:searchable])
-      add_solr_value(solr_doc, "identifier", sourceId, :string, [:searchable])
-      add_solr_value(solr_doc, "source_id", sourceId, :string, [:searchable])
+
+    define_template :creative_commons do |xml|
+      xml.use {
+        xml.human(:type => "creativeCommons")
+        xml.machine(:type => "creativeCommons")
+      }
     end
-    solr_doc
-  end
-end #class
+
+    def self.xml_template
+      Nokogiri::XML::Builder.new do |xml|
+        xml.rightsMetadata{
+          xml.access(:type => 'discover'){
+            xml.machine{ xml.none }
+          }
+          xml.access(:type => 'read'){
+            xml.machine{ xml.none }
+          }
+          xml.use{
+            xml.human(:type => 'useAndReproduction')
+            xml.human(:type => "creativeCommons")
+            xml.machine(:type => "creativeCommons")
+          }
+          xml.copyright{ xml.human }
+        }
+      end.doc
+    end
+
+    def to_solr(solr_doc=Hash.new, *args)
+      super(solr_doc, *args)
+      # ...
+      solr_doc
+    end
+
+  end #class
 end
--- spec/dor/rights_metadata_spec.rb
+++ spec/dor/rights_metadata_spec.rb
@@ -1,6 +1,6 @@
-require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require 'spec_helper'
 
-class RightsHaver < ActiveFedora::Base
+class RightsHaver < Dor::Item
 #   include Dor::HasRightsMD
 end
 
@@ -8,22 +8,32 @@ describe Dor::RightsMetadataDS do
   before(:each) { stub_config }
   after(:each)  { unstub_config }
 
-  let(:item) do
-    item = instantiate_fixture('druid:oo201oo0001', RightsHaver)
-    allow(item).to receive(:new?).and_return(false)
-    ds = item.rightsMetadata
-    ds.instance_variable_set(:@datastream_content, item.rightsMetadata.content)
+  before(:each) do
+    @item = instantiate_fixture('druid:oo201oo0001', RightsHaver)
+    allow(@item).to receive(:new?).and_return(false)
+    allow(@item).to receive(:workflows).and_return(double())
+    ds = @item.rightsMetadata
+    ds.instance_variable_set(:@datastream_content, @item.rightsMetadata.content)
     allow(ds).to receive(:new?).and_return(false)
-    item
+    allow(Dor::Item).to receive(:find).with('druid:oo201oo0001').and_return(@item)
   end
 
-  it "should have an rightsMetadata datastream" do
-    expect(item.datastreams['rightsMetadata']).to be_a(Dor::RightsMetadataDS)
+  it "#new" do
+    expect(Dor::RightsMetadataDS.new).to be_a(Dor::RightsMetadataDS)
+  end
+
+  it "should have a rightsMetadata datastream" do
+    expect(@item).to be_a(RightsHaver)
+    expect(@item).to be_kind_of(Dor::Item)
+    skip "Not fully written"
+    #  binding.pry
+    expect(@item.datastreams['rightsMetadata']).to be_a(Dor::RightsMetadataDS)
   end
 
   describe 'to_solr' do
     it 'should have correct primary' do
-      doc=item.to_solr
+      skip "Not fully written"
+      doc=@item.to_solr
       #doc.keys.sort.each do |key|
       #  puts "#{key} #{doc[key]}"
       #end
