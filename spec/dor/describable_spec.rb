bd55436da343ad0a97ed067df7ddf6a723418d71
Joe Atzberger <ohiocore@gmail.com>
2015-04-24 17:43:07 -0700

    DRY up shared xml

--- spec/dor/describable_spec.rb
+++ spec/dor/describable_spec.rb
@@ -52,19 +52,17 @@ describe Dor::Describable do
     allow(@item).to receive(:find_metadata_file).and_return(nil)
     Dor::MetadataService.class_eval { class << self; alias_method :_fetch, :fetch; end }
     expect(Dor::MetadataService).to receive(:fetch).with('barcode:36105049267078') { Dor::MetadataService._fetch('barcode:36105049267078') }
-    expect(@item.datastreams['descMetadata'].ng_xml.to_s).to be_equivalent_to('<?xml version="1.0"?>
-          <mods xmlns="http://www.loc.gov/mods/v3" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" version="3.3" xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-3.xsd">
-            <titleInfo>
-              <title/>
-            </titleInfo>
-          </mods>')
+    xml = <<-END_OF_XML
+    <?xml version="1.0"?>
+    <mods xmlns="http://www.loc.gov/mods/v3" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" version="3.3" xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-3.xsd">
+      <titleInfo>
+        <title/>
+      </titleInfo>
+    </mods>
+    END_OF_XML
+    expect(@item.datastreams['descMetadata'].ng_xml.to_s).to be_equivalent_to(xml)
     @item.build_datastream('descMetadata')
-    expect(@item.datastreams['descMetadata'].ng_xml.to_s).not_to be_equivalent_to('<?xml version="1.0"?>
-          <mods xmlns="http://www.loc.gov/mods/v3" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" version="3.3" xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-3.xsd">
-            <titleInfo>
-              <title/>
-            </titleInfo>
-          </mods>')
+    expect(@item.datastreams['descMetadata'].ng_xml.to_s).not_to be_equivalent_to(xml)
   end
 
   it "produces dublin core from the MODS in the descMetadata datastream" do
