1c9151a3f69e3162bada30b8bd23453dbcf19f26
Willy Mene <wmene@stanford.edu>
2015-04-24 17:57:14 -0700

    Added Sdr::Client with method to get current_version

---
+++ lib/dor/utils/sdr_client.rb
@@ -0,0 +1,23 @@
+module Sdr
+
+  module Client
+    class << self
+
+      # @param [String] druid id of the object you want the version of
+      # @returns [Integer] the current version from SDR
+      def current_version druid
+        sdr_client = Dor::Config.sdr.rest_client
+        xml = sdr_client["objects/#{druid}/current_version"].get
+
+        begin
+          doc = Nokogiri::XML xml
+          raise if doc.root.name != 'currentVersion'
+          return Integer(doc.text)
+        rescue => e
+          raise "Unable to parse XML from SDR current_version API call: #{xml}"
+        end
+      end
+
+    end
+  end
+end
---
+++ spec/utils/sdr_client_spec.rb
@@ -0,0 +1,37 @@
+require "spec_helper"
+require "fakeweb"
+
+require 'dor/utils/sdr_client'
+
+describe Sdr::Client do
+
+  describe '.get_current_version' do
+    let(:sdr_client) { Dor::Config.sdr.rest_client }
+
+    it 'returns the current of the object from SDR' do
+      FakeWeb.register_uri(:get, "#{sdr_client.url}/objects/druid:ab123cd4567/current_version",
+                           :body => "<currentVersion>2</currentVersion>")
+      expect(Sdr::Client.current_version('druid:ab123cd4567')).to eq 2
+    end
+
+    context 'raises an exception if the xml' do
+
+      it 'has the wrong root element' do
+        FakeWeb.register_uri(:get, "#{sdr_client.url}/objects/druid:ab123cd4567/current_version",
+                             :body => "<wrongRoot>2</wrongRoot>")
+        expect{ Sdr::Client.current_version('druid:ab123cd4567') }.to raise_error(Exception,
+                                                "Unable to parse XML from SDR current_version API call: <wrongRoot>2</wrongRoot>")
+      end
+
+      it 'does not contain an Integer as its text' do
+        FakeWeb.register_uri(:get, "#{sdr_client.url}/objects/druid:ab123cd4567/current_version",
+                             :body => "<currentVersion>two</currentVersion>")
+        expect{ Sdr::Client.current_version('druid:ab123cd4567') }.to raise_error(Exception,
+                                                "Unable to parse XML from SDR current_version API call: <currentVersion>two</currentVersion>")
+      end
+
+    end
+
+  end
+
+end
