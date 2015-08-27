3134f2fe160ac219081e8b6e96bf79025d513d3d
Darren Hardy <drh@stanford.edu>
2015-08-27 14:25:41 -0700

    initial version of a republish scriptie that uses dor-services-app

---
+++ script/republish.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/env ruby
+#
+# republish.rb - Executes republish via dor-services-app REST API.
+#
+require 'uri'
+require 'yaml'
+require 'rest-client'
+
+if ARGV.first == '--help' || ARGV.size < 1
+  puts 'Usage: republish.rb druid1 [... druid2 ...]'
+  exit(-1)
+end
+
+CONF_FN = 'config/republish.yml'
+
+fail "Must have configuration file #{CONF_FN}" unless File.size?(CONF_FN)
+config = YAML.load(File.read(CONF_FN))
+fail 'Invalid YAML configuration' if config['url'].nil?
+
+base_uri = URI(config['url'])
+fail 'Must be an https URL' unless base_uri.scheme == 'https'
+fail 'Must use user/password authentication' if base_uri.user.nil? || base_uri.password.nil?
+
+endpoint = RestClient::Resource.new(base_uri.to_s)
+
+ARGV.each do |druid|
+  begin
+    druid = "druid:#{druid}" unless druid =~ /^druid:/
+
+    # POST to dor-services-app's REST API
+    endpoint["v1/objects/#{druid}/publish"].post('') do |response|
+      puts "#{druid}: #{response.code} code from republish"
+      puts "#{druid}: ERROR: unexpected status code" unless response.code == 200
+    end
+  rescue => e
+    puts "#{druid}: ERROR: cannot republish: #{e.message}"
+  end
+end
+
+exit(0)
+  
