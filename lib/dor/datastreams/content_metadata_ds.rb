228299e34fc342e96111d93d04e38ab4ca36ba99
Darren Hardy <drh@stanford.edu>
2015-09-02 14:00:55 -0700

    add comments

--- lib/dor/datastreams/content_metadata_ds.rb
+++ lib/dor/datastreams/content_metadata_ds.rb
@@ -66,7 +66,17 @@ module Dor
       self.save
     end
     
-    # <externalFile objectId="druid:mn123pq4567" resourceId="Image01" fileid="image_01.jp2000" />
+    #
+    # Generates the XML tree for externalFile references. For example,
+    #
+    #     <externalFile objectId="druid:mn123pq4567" resourceId="Image01" fileid="image_01.jp2000" />
+    #
+    # @param [String] objectId the linked druid
+    # @param [String] resourceId the linked druid's resource identifier
+    # @param [String] fileId the linked druid's resource's file identifier
+    #
+    # @return [Nokogiri::XML::Element]
+    #
     def generate_external_file_node(objectId, resourceId, fileId)
       externalFile = Nokogiri::XML::Element.new('externalFile', self.ng_xml)
       externalFile[:objectId] = objectId
@@ -75,7 +85,15 @@ module Dor
       externalFile
     end
     
-    # <relationship type="alsoAvailableAs" objectId="[map-druid]" />
+    #
+    # Generates the XML tree for virtual resource relationship reference. For example,
+    #
+    #     <relationship type="alsoAvailableAs" objectId="druid:mn123pq4567" />
+    #
+    # @param [String] objectId the linked druid
+    #
+    # @return [Nokogiri::XML::Element]
+    #
     def generate_also_available_as_node(objectId)
       relationship = Nokogiri::XML::Element.new('relationship', self.ng_xml)
       relationship[:type] = 'alsoAvailableAs'
@@ -83,24 +101,36 @@ module Dor
       relationship
     end
 
-    # copies the child's resource into the parent as a virtual resource
-    # assumes the resource isn't a duplicate of an existing virtual or real resource
+    #
+    # Copies the child's resource into the parent (self) as a virtual resource.
+    # Assumes the resource isn't a duplicate of an existing virtual or real resource.
+    # 
+    # @param [Dor::Item] child
+    # @param [Nokogiri::XML::Element] child_resource
+    #
+    # @return [Nokogiri::XML::Element] the new resource that was added to the contentMetadata
+    #
     def add_virtual_resource(child, child_resource)
-      max = self.ng_xml.search('//resource').map{ |node| node[:sequence].to_i }.max
+      # create a virtual resource element with attributes linked to the child
+      sequence_max = self.ng_xml.search('//resource').map{ |node| node[:sequence].to_i }.max
       resource = Nokogiri::XML::Element.new('resource', self.ng_xml)
       resource[:type] = child_resource[:type]
-      resource[:sequence] = max + 1
+      resource[:sequence] = sequence_max + 1
       resource[:id] = child_resource[:id]
       
       label = Nokogiri::XML::Element.new('label', self.ng_xml)
       label.content = child_resource.search('label').first.content
       resource << label
       
+      # iterate over all the published files and link to them
       child_resource.search('file[@publish=\'yes\']').each do |file|
         resource << generate_external_file_node(child.pid, child_resource[:id], file[:id])
       end
       resource << generate_also_available_as_node(child.pid)
+      
+      # save the virtual resource as a sibling and return
       self.ng_xml.root << resource
+      resource
     end
     
     def add_resource(files,resource_name, position,type="file")
