daa33eb6c438c9c65cc6dad762ffe48ff40224f7
Darren Hardy <drh@stanford.edu>
2015-09-02 15:09:20 -0700

    use https for manifest.json logos

--- lib/dor/models/presentable.rb
+++ lib/dor/models/presentable.rb
@@ -33,10 +33,10 @@ module Dor
         'label' => lbl,
         'attribution' => 'Provided by the Stanford University Libraries',
         'logo' => {
-          '@id' => "http://stacks.stanford.edu/image/iiif/wy534zh7137%2FSULAIR_rosette/full/400,/0/default.jpg",
+          '@id' => "https://stacks.stanford.edu/image/iiif/wy534zh7137%2FSULAIR_rosette/full/400,/0/default.jpg",
           'service' => {
             '@context' => "http://iiif.io/api/image/2/context.json",
-            '@id' => "http://stacks.stanford.edu/image/iiif/wy534zh7137%2FSULAIR_rosette",
+            '@id' => "https://stacks.stanford.edu/image/iiif/wy534zh7137%2FSULAIR_rosette",
             'profile' => "http://iiif.io/api/image/2/level1.json"
             }
           },
--- spec/dor/presentable_spec.rb
+++ spec/dor/presentable_spec.rb
@@ -70,10 +70,10 @@ describe Dor::Presentable do
       "label": "Roman Imperial denarius",
       "attribution": "(c) 2009 by Jasper Wilcox. All rights reserved.",
       "logo": {
-        "@id": "http://stacks.stanford.edu/image/iiif/wy534zh7137%2FSULAIR_rosette/full/400,/0/default.jpg",
+        "@id": "https://stacks.stanford.edu/image/iiif/wy534zh7137%2FSULAIR_rosette/full/400,/0/default.jpg",
         "service": {
           "@context": "http://iiif.io/api/image/2/context.json",
-          "@id": "http://stacks.stanford.edu/image/iiif/wy534zh7137%2FSULAIR_rosette",
+          "@id": "https://stacks.stanford.edu/image/iiif/wy534zh7137%2FSULAIR_rosette",
           "profile": "http://iiif.io/api/image/2/level1.json"
         }
       },
