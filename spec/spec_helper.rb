653a6bcc6292b202b75ad73568de9beba1c39827
Joe Atzberger <ohiocore@gmail.com>
2015-09-02 15:46:14 -0700

    Cleanup spec_helper
    Tons of require statements that mean nothing.  Depressing.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,18 +3,9 @@ SimpleCov.start
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
-require 'bundler/setup'
 require 'rspec'
-require 'awesome_print'
-#require 'rspec/autorun'
-#require 'rspec/mocks'
-
 require 'solrizer'
-
-require 'rubygems'
-require 'rake'
 require 'dor-services'
-#require 'ruby-debug'
 require 'foxml_helper'
 require 'equivalent-xml'
 require 'fakeweb'
@@ -27,21 +18,21 @@ require 'nokogiri'
 module Dor::SpecHelpers
 
   def stub_config
-    @fixture_dir = fixture_dir = File.join(File.dirname(__FILE__),"fixtures")
+    @fixture_dir = fixture_dir = File.join(File.dirname(__FILE__), 'fixtures')
     Dor::Config.push! do
       suri.mint_ids false
       gsearch do
-        url "http://solr.edu/gsearch"
-        rest_url "http://fedora.edu/gsearch/rest"
+        url      'http://solr.edu/gsearch'
+        rest_url 'http://fedora.edu/gsearch/rest'
       end
-      solrizer.url "http://solr.edu/solrizer"
-      fedora.url "http://fedora.edu/fedora"
-      stacks.local_workspace_root File.join(fixture_dir, "workspace")
-      stacks.local_stacks_root File.join(fixture_dir, "stacks")
-      stacks.local_document_cache_root File.join(fixture_dir, "purl")
-      sdr.local_workspace_root         File.join(fixture_dir, "workspace")
-      sdr.local_export_home            File.join(fixture_dir, "export")
-      stacks.document_cache_host "purl-test.stanford.edu"
+      solrizer.url 'http://solr.edu/solrizer'
+      fedora.url   'http://fedora.edu/fedora'
+      stacks.local_workspace_root      File.join(fixture_dir, 'workspace')
+      stacks.local_stacks_root         File.join(fixture_dir, 'stacks')
+      stacks.local_document_cache_root File.join(fixture_dir, 'purl')
+      sdr.local_workspace_root         File.join(fixture_dir, 'workspace')
+      sdr.local_export_home            File.join(fixture_dir, 'export')
+      stacks.document_cache_host 'purl-test.stanford.edu'
     end
     ActiveFedora.stub(:fedora).and_return(double('frepo').as_null_object)
   end
@@ -91,12 +82,11 @@ module Kernel
 end
 
 require 'dor_config'
-
- require 'vcr'
- VCR.configure do |c|
-    c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
-   c.hook_into :webmock
-   c.allow_http_connections_when_no_cassette = true
-   c.default_cassette_options = { :record => :new_episodes }
-   c.configure_rspec_metadata!
- end
+require 'vcr'
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  c.hook_into :webmock
+  c.allow_http_connections_when_no_cassette = true
+  c.default_cassette_options = { :record => :new_episodes }
+  c.configure_rspec_metadata!
+end
