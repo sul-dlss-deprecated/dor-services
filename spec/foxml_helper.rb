7f41f4fb5701056a25997113018e360d644f9796
Joe Atzberger <ohiocore@gmail.com>
2015-09-14 17:44:12 -0700

    Apply proper formatting to foxml_helper
    Also comment out loop around commented-out payload.  No point doing
    nothing many times.

--- spec/foxml_helper.rb
+++ spec/foxml_helper.rb
@@ -5,44 +5,44 @@ def item_from_foxml(foxml, item_class = Dor::Base)
     [node['NAME'].split(/#/).last, node['VALUE']]
   }]
   result = item_class.new(:pid => foxml.root['PID'])
-  result.label = properties['label']
+  result.label    = properties['label']
   result.owner_id = properties['ownerId']
   xml_streams.each do |stream|
     begin
-    content = stream.xpath('.//foxml:xmlContent/*').first.to_xml
-    dsid = stream['ID']
-    ds = result.datastreams[dsid]
-    if ds.nil?
-      ds = ActiveFedora::OmDatastream.new(result,dsid)
-      result.add_datastream(ds)
-    end
+      content = stream.xpath('.//foxml:xmlContent/*').first.to_xml
+      dsid = stream['ID']
+      ds = result.datastreams[dsid]
+      if ds.nil?
+        ds = ActiveFedora::OmDatastream.new(result, dsid)
+        result.add_datastream(ds)
+      end
 
-    if ds.is_a?(ActiveFedora::OmDatastream)
-      result.datastreams[dsid] = ds.class.from_xml(Nokogiri::XML(content), ds)
-    elsif ds.is_a?(ActiveFedora::RelsExtDatastream)
-      result.datastreams[dsid] = ds.class.from_xml(content, ds)
-    else
-      result.datastreams[dsid] = ds.class.from_xml(ds, stream)
+      if ds.is_a?(ActiveFedora::OmDatastream)
+        result.datastreams[dsid] = ds.class.from_xml(Nokogiri::XML(content), ds)
+      elsif ds.is_a?(ActiveFedora::RelsExtDatastream)
+        result.datastreams[dsid] = ds.class.from_xml(content, ds)
+      else
+        result.datastreams[dsid] = ds.class.from_xml(ds, stream)
+      end
+    rescue
+      # TODO: (?) rescue if 1 datastream failed
     end
-  rescue
-    #rescue if 1 datastream failed
-  end
   end
 
   # stub item and datastream repo access methods
-  result.datastreams.each_pair do |dsid,ds|
-    if ds.is_a?(ActiveFedora::OmDatastream) && !ds.is_a?(Dor::WorkflowDs)
-      ds.instance_eval do
-#        def content       ; self.ng_xml.to_s                 ; end
-#        def content=(val) ; self.ng_xml = Nokogiri::XML(val) ; end
-      end
-    end
+  result.datastreams.each_pair do |dsid, ds|
+    # if ds.is_a?(ActiveFedora::OmDatastream) && !ds.is_a?(Dor::WorkflowDs)
+    #   ds.instance_eval do
+    #     def content       ; self.ng_xml.to_s                 ; end
+    #     def content=(val) ; self.ng_xml = Nokogiri::XML(val) ; end
+    #   end
+    # end
     ds.instance_eval do
-      def save          ; return true                      ; end
+      def save ; true ; end
     end
   end
   result.instance_eval do
-    def save ; return true ; end
+    def save ; true ; end
   end
   result
 end
