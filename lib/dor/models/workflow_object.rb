6f9afd803517a2489e871932f48eb762080ad73e
Joe Atzberger <ohiocore@gmail.com>
2015-09-20 14:27:10 -0500

    Rubocop cleanup, remove redundant self references

--- lib/dor/models/workflow_object.rb
+++ lib/dor/models/workflow_object.rb
@@ -5,7 +5,7 @@ module Dor
     include Identifiable
     include SolrDocHelper
     include Governable
-    @@xml_cache = {}
+    @@xml_cache  = {}
     @@repo_cache = {}
 
     has_object_type 'workflow'
@@ -22,7 +22,6 @@ module Dor
     # @return [String] the initial workflow xml
     def self.initial_workflow(name)
       return @@xml_cache[name] if @@xml_cache.include?(name)
-
       find_and_cache_workflow_xml_and_repo name
       @@xml_cache[name]
     end
@@ -33,7 +32,6 @@ module Dor
     # @return [String] the initial workflow xml
     def self.initial_repo(name)
       return @@repo_cache[name] if @@repo_cache.include?(name)
-
       find_and_cache_workflow_xml_and_repo name
       @@repo_cache[name]
     end
@@ -63,12 +61,13 @@ module Dor
 
     # Searches DOR for the workflow definition object.  It then caches the workflow repository and xml
     # @param [String] name the name of the workflow
+    # @return [Object] a Dor::xxxx object, e.g. a Dor::Item object
     def self.find_and_cache_workflow_xml_and_repo(name)
       wobj = find_by_name(name)
       raise "Failed to find workflow via find_by_name('#{name}')" if wobj.nil?
-      wf_xml = wobj.generate_initial_workflow
       @@repo_cache[name] = wobj.definition.repo
-      @@xml_cache[name] = wf_xml
+      @@xml_cache[name]  = wobj.generate_initial_workflow
+      wobj
     end
 
   end
