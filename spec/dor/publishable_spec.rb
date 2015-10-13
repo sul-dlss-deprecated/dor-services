d6fbcbc4f33aeee70b708afad07361320948ce2e
Laney McGlohon <laneymcg@stanford.edu>
2015-10-13 13:01:05 -0700

    Removed unnecessary addition of release tag in the identityMetadata

--- spec/dor/publishable_spec.rb
+++ spec/dor/publishable_spec.rb
@@ -7,12 +7,10 @@ class PublishableItem < ActiveFedora::Base
 end
 
 describe Dor::Publishable do
-
   before(:each) { stub_config   }
   after(:each)  { unstub_config }
 
   before :each do
-
     @item = instantiate_fixture('druid:ab123cd4567', PublishableItem)
     @apo  = instantiate_fixture('druid:fg890hi1234', Dor::AdminPolicyObject)
     @item.stub(:admin_policy_object).and_return(@apo)
@@ -62,14 +60,14 @@ describe Dor::Publishable do
     allow(OpenURI).to receive(:open_uri).with('https://purl-test.stanford.edu/ab123cd4567.xml').and_return('<xml/>')
   end
 
-  it "has a rightsMetadata datastream" do
+  it 'has a rightsMetadata datastream' do
     @item.datastreams['rightsMetadata'].should be_a(ActiveFedora::OmDatastream)
   end
 
-  it "should provide a rightsMetadata datastream builder" do
+  it 'should provide a rightsMetadata datastream builder' do
     rights_md = @apo.defaultObjectRights.content
     @item.datastreams['rightsMetadata'].ng_xml.to_s.should_not be_equivalent_to(rights_md)
-    @item.build_datastream('rightsMetadata',true)
+    @item.build_datastream('rightsMetadata', true)
     @item.datastreams['rightsMetadata'].ng_xml.to_s.should be_equivalent_to(rights_md)
   end
 
@@ -108,7 +106,7 @@ describe Dor::Publishable do
 
       it 'relationships' do
         ns = { 'rdf' => 'http://www.w3.org/1999/02/22-rdf-syntax-ns#', 'hydra' => 'http://projecthydra.org/ns/relations#',
-          'fedora' => 'info:fedora/fedora-system:def/relations-external#', 'fedora-model' => 'info:fedora/fedora-system:def/model#' }
+               'fedora' => 'info:fedora/fedora-system:def/relations-external#', 'fedora-model' => 'info:fedora/fedora-system:def/model#' }
         expect(@p_xml.at_xpath('/publicObject/rdf:RDF', ns)).to be
         expect(@p_xml.at_xpath('/publicObject/rdf:RDF/rdf:Description/fedora:isMemberOf', ns)).to be
         expect(@p_xml.at_xpath('/publicObject/rdf:RDF/rdf:Description/fedora:isMemberOfCollection', ns)).to be
@@ -123,73 +121,76 @@ describe Dor::Publishable do
         expect(@item.datastreams['RELS-EXT'].content).to be_equivalent_to @rels
       end
 
-       it "an encoding of UTF-8" do
-         @p_xml.encoding.should =~ /UTF-8/
-       end
+      it 'an encoding of UTF-8' do
+        @p_xml.encoding.should =~ /UTF-8/
+      end
 
-       it "an id attribute" do
-         @p_xml.at_xpath('/publicObject/@id').value.should =~ /^druid:ab123cd4567/
-       end
+      it 'an id attribute' do
+        @p_xml.at_xpath('/publicObject/@id').value.should =~ /^druid:ab123cd4567/
+      end
 
-       it "a published attribute" do
-         @p_xml.at_xpath('/publicObject/@published').value.should == @now.xmlschema
-       end
+      it 'a published attribute' do
+        @p_xml.at_xpath('/publicObject/@published').value.should == @now.xmlschema
+      end
 
-       it "a published version" do
-         expect(@p_xml.at_xpath('/publicObject/@publishVersion').value).to eq('dor-services/' + Dor::VERSION)
-       end
+      it 'a published version' do
+        expect(@p_xml.at_xpath('/publicObject/@publishVersion').value).to eq('dor-services/' + Dor::VERSION)
+      end
 
-       it "identityMetadata" do
-         @p_xml.at_xpath('/publicObject/identityMetadata').should be
-       end
+      it 'identityMetadata' do
+        @p_xml.at_xpath('/publicObject/identityMetadata').should be
+      end
 
-       it "contentMetadata" do
-         @p_xml.at_xpath('/publicObject/contentMetadata').should be
-       end
+      it 'contentMetadata' do
+        @p_xml.at_xpath('/publicObject/contentMetadata').should be
+      end
 
-       it "rightsMetadata" do
-         @p_xml.at_xpath('/publicObject/rightsMetadata').should be
-       end
+      it 'rightsMetadata' do
+        @p_xml.at_xpath('/publicObject/rightsMetadata').should be
+      end
 
-       it "generated dublin core" do
-         @p_xml.at_xpath('/publicObject/oai_dc:dc', 'oai_dc' => 'http://www.openarchives.org/OAI/2.0/oai_dc/').should be
-       end
+      it 'generated dublin core' do
+        @p_xml.at_xpath('/publicObject/oai_dc:dc', 'oai_dc' => 'http://www.openarchives.org/OAI/2.0/oai_dc/').should be
+      end
 
-       it "relationships" do
-         ns = { 'rdf' => 'http://www.w3.org/1999/02/22-rdf-syntax-ns#', 'hydra' => 'http://projecthydra.org/ns/relations#',
-           'fedora' => 'info:fedora/fedora-system:def/relations-external#', 'fedora-model' => 'info:fedora/fedora-system:def/model#' }
-         @p_xml.at_xpath('/publicObject/rdf:RDF', ns).should be
-         @p_xml.at_xpath('/publicObject/rdf:RDF/rdf:Description/fedora:isMemberOf', ns).should be
-         @p_xml.at_xpath('/publicObject/rdf:RDF/rdf:Description/fedora:isMemberOfCollection', ns).should be
-         @p_xml.at_xpath('/publicObject/rdf:RDF/rdf:Description/fedora-model:hasModel', ns).should_not be
-         @p_xml.at_xpath('/publicObject/rdf:RDF/rdf:Description/hydra:isGovernedBy', ns).should_not be
-       end
+      it 'relationships' do
+        ns = { 'rdf' => 'http://www.w3.org/1999/02/22-rdf-syntax-ns#', 'hydra' => 'http://projecthydra.org/ns/relations#',
+               'fedora' => 'info:fedora/fedora-system:def/relations-external#', 'fedora-model' => 'info:fedora/fedora-system:def/model#' }
+        @p_xml.at_xpath('/publicObject/rdf:RDF', ns).should be
+        @p_xml.at_xpath('/publicObject/rdf:RDF/rdf:Description/fedora:isMemberOf', ns).should be
+        @p_xml.at_xpath('/publicObject/rdf:RDF/rdf:Description/fedora:isMemberOfCollection', ns).should be
+        @p_xml.at_xpath('/publicObject/rdf:RDF/rdf:Description/fedora-model:hasModel', ns).should_not be
+        @p_xml.at_xpath('/publicObject/rdf:RDF/rdf:Description/hydra:isGovernedBy', ns).should_not be
+      end
 
-       it "clones of the content of the other datastreams, keeping the originals in tact" do
-         @item.datastreams['identityMetadata'].ng_xml.at_xpath("/identityMetadata").should be
-         @item.datastreams['contentMetadata'].ng_xml.at_xpath("/contentMetadata").should be
-         @item.datastreams['rightsMetadata'].ng_xml.at_xpath("/rightsMetadata").should be
-         @item.datastreams['RELS-EXT'].content.should be_equivalent_to @rels
-       end
+      it 'clones of the content of the other datastreams, keeping the originals in tact' do
+        @item.datastreams['identityMetadata'].ng_xml.at_xpath('/identityMetadata').should be
+        @item.datastreams['contentMetadata'].ng_xml.at_xpath('/contentMetadata').should be
+        @item.datastreams['rightsMetadata'].ng_xml.at_xpath('/rightsMetadata').should be
+        @item.datastreams['RELS-EXT'].content.should be_equivalent_to @rels
+      end
 
-       it "does not include a releaseData element when there are no release tags" do
-         expect(@p_xml.at_xpath('/publicObject/releaseData')).to be nil
-       end
+      it 'does not include a releaseData element when there are no release tags' do
+        expect(@p_xml.at_xpath('/publicObject/releaseData')).to be nil
+      end
 
-       it "does include a releaseData element when there is content inside it" do
-         #Fake a tag with at least one children
-         releaseData = "<releaseData><release>foo</release></releaseData>"
-         allow(@item).to receive(:generate_release_xml).and_return(releaseData)
-         p_xml = Nokogiri::XML(@item.public_xml)
-         expect(p_xml.at_xpath('/publicObject/releaseData')).to be
-       end
+      it 'does include a releaseData element when there is content inside it' do
+        # Fake a tag with at least one children
+        releaseData = '<releaseData><release>foo</release></releaseData>'
+        allow(@item).to receive(:generate_release_xml).and_return(releaseData)
+        p_xml = Nokogiri::XML(@item.public_xml)
+        expect(p_xml.at_xpath('/publicObject/releaseData')).to be
+      end
 
+      it 'does not include a release element in the identityMetadata' do
+        @item.datastreams['identityMetadata'].ng_xml = "<identityMetadata><release displayType=\"file\" release=\"true\" to=\"Searchworks\" what=\"collection\" when=\"2015-09-02T19:12:23Z\" who=\"laneymcg\">true</release></identityMetadata>"
+        p_xml = Nokogiri::XML(@item.public_xml)
+        expect(p_xml.at_xpath('/publicObject/identityMetadata/release')).to be nil
+      end
     end
 
-    describe "#publish_metadata" do
-
-      context "with no world discover access in rightsMetadata" do
-
+    describe '#publish_metadata' do
+      context 'with no world discover access in rightsMetadata' do
         let(:purl_root) { Dir.mktmpdir }
 
         before(:each) do
@@ -211,7 +212,7 @@ describe Dor::Publishable do
             </rightsMetadata>
           EOXML
 
-          Dor::Config.push! {|config| config.stacks.local_document_cache_root purl_root}
+          Dor::Config.push! { |config| config.stacks.local_document_cache_root purl_root }
         end
 
         after(:each) do
@@ -219,7 +220,7 @@ describe Dor::Publishable do
           Dor::Config.pop!
         end
 
-        it "does not publish the object" do
+        it 'does not publish the object' do
           Dor::DigitalStacksService.should_not_receive(:transfer_to_document_store)
           @item.publish_metadata
         end
@@ -228,14 +229,13 @@ describe Dor::Publishable do
           # create druid tree and content in purl root
           druid1 = DruidTools::Druid.new @item.pid, purl_root
           druid1.mkdir
-          File.open(File.join(druid1.path, 'tmpfile'), 'w') {|f| f.write 'junk' }
+          File.open(File.join(druid1.path, 'tmpfile'), 'w') { |f| f.write 'junk' }
           @item.publish_metadata
           expect(File).to_not exist(druid1.path)
         end
       end
 
-      context "copies to the document cache" do
-
+      context 'copies to the document cache' do
         before(:each) do
           Dor::DigitalStacksService.should_receive(:transfer_to_document_store).with('druid:ab123cd4567', /<identityMetadata/, 'identityMetadata')
           Dor::DigitalStacksService.should_receive(:transfer_to_document_store).with('druid:ab123cd4567', /<contentMetadata/, 'contentMetadata')
@@ -245,33 +245,32 @@ describe Dor::Publishable do
           Dor::DigitalStacksService.should_receive(:transfer_to_document_store).with('druid:ab123cd4567', /<mods:mods/, 'mods')
         end
 
-        it "identityMetadta, contentMetadata, rightsMetadata, generated dublin core, and public xml" do
+        it 'identityMetadata, contentMetadata, rightsMetadata, generated dublin core, and public xml' do
           @item.rightsMetadata.content = "<rightsMetadata><access type='discover'><machine><world/></machine></access></rightsMetadata>"
           @item.publish_metadata
         end
 
-        it "even when rightsMetadata uses xml namespaces" do
+        it 'even when rightsMetadata uses xml namespaces' do
           @item.rightsMetadata.content = %q(<rightsMetadata xmlns="http://hydra-collab.stanford.edu/schemas/rightsMetadata/v1"><access type='discover'><machine><world/></machine></access></rightsMetadata>)
           @item.publish_metadata
         end
       end
     end
 
-    context "error handling" do
-      it "throws an exception if any of the required datastreams are missing" do
+    context 'error handling' do
+      it 'throws an exception if any of the required datastreams are missing' do
         pending
       end
     end
   end
   describe 'publish remotely' do
-  before(:each) do
-    Dor::Config.push! {|config| config.dor_services.url 'https://lyberservices-test.stanford.edu/dor'}
+    before(:each) do
+      Dor::Config.push! { |config| config.dor_services.url 'https://lyberservices-test.stanford.edu/dor' }
 
-    RestClient::Resource.any_instance.stub(:post)
+      RestClient::Resource.any_instance.stub(:post)
+    end
+    it 'should hit the correct url' do
+      @item.publish_metadata_remotely.should == 'https://lyberservices-test.stanford.edu/dor/v1/objects/druid:ab123cd4567/publish'
+    end
   end
-  it 'should hit the correct url' do
-    @item.publish_metadata_remotely.should == 'https://lyberservices-test.stanford.edu/dor/v1/objects/druid:ab123cd4567/publish'
-  end
-  end
-
 end
