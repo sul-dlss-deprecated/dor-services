8fad8c5cf983d8dd5749ea70e1cb62975160dafb
Joe Atzberger <ohiocore@gmail.com>
2015-10-22 19:05:47 -0700

    Rubocopping Identifiable

--- lib/dor/models/identifiable.rb
+++ lib/dor/models/identifiable.rb
@@ -6,8 +6,8 @@ module Dor
     include Upgradable
 
     included do
-      has_metadata :name => "DC", :type => SimpleDublinCoreDs, :label => 'Dublin Core Record for self object'
-      has_metadata :name => "identityMetadata", :type => Dor::IdentityMetadataDS, :label => 'Identity Metadata'
+      has_metadata :name => 'DC', :type => SimpleDublinCoreDs, :label => 'Dublin Core Record for self object'
+      has_metadata :name => 'identityMetadata', :type => Dor::IdentityMetadataDS, :label => 'Identity Metadata'
     end
 
     module ClassMethods
@@ -20,9 +20,7 @@ module Dor
 
     def initialize attrs = {}
       if Dor::Config.suri.mint_ids
-        unless attrs[:pid]
-          attrs = attrs.merge!({:pid=>Dor::SuriService.mint_id, :new_object => true})
-        end
+        attrs = attrs.merge!({:pid => Dor::SuriService.mint_id, :new_object => true}) unless attrs[:pid]
       end
       super
     end
@@ -34,8 +32,8 @@ module Dor
 
     # helper method to get just the content type tag
     def content_type_tag
-      content_tag=tags.select {|tag| tag.include?('Process : Content Type')}
-      content_tag.size == 1 ? content_tag[0].split(':').last.strip : ""
+      content_tag = tags.select {|tag| tag.include?('Process : Content Type')}
+      content_tag.size == 1 ? content_tag[0].split(':').last.strip : ''
     end
 
     # Syntactic sugar for identifying applied DOR Concerns
@@ -53,120 +51,113 @@ module Dor
       end
     end
 
-    @@collection_hash={}
-    @@apo_hash={}
-    @@hydrus_apo_hash={}
-    @@hydrus_collection_hash={}
-    def to_solr(solr_doc = Hash.new, *args)
+    @@collection_hash = {}
+    @@apo_hash        = {}
+    @@hydrus_apo_hash = {}
+    @@hydrus_collection_hash = {}
+    def to_solr(solr_doc = {}, *args)
       assert_content_model
       super(solr_doc)
       solr_doc[Dor::INDEX_VERSION_FIELD] = Dor::VERSION
-      solr_doc[solr_name('indexed_at',:date)] = Time.now.utc.xmlschema
+      solr_doc[solr_name('indexed_at', :date)] = Time.now.utc.xmlschema
       add_solr_value(solr_doc, 'indexed_day', Time.now.beginning_of_day.utc.xmlschema, :string, [:searchable, :facetable])
       datastreams.values.each do |ds|
-        unless ds.new?
-          add_solr_value(solr_doc,'ds_specs',ds.datastream_spec_string,:string,[:displayable])
-        end
+        next if ds.new?
+        add_solr_value(solr_doc, 'ds_specs', ds.datastream_spec_string, :string, [:displayable])
       end
       add_solr_value(solr_doc, 'title_sort', label, :string, [:sortable])
       rels_doc = Nokogiri::XML(datastreams['RELS-EXT'].content)
-      apos=rels_doc.search('//rdf:RDF/rdf:Description/hydra:isGovernedBy','hydra' => 'http://projecthydra.org/ns/relations#', 'fedora' => 'info:fedora/fedora-system:def/relations-external#', 'rdf' => 'http://www.w3.org/1999/02/22-rdf-syntax-ns#' 	)
+      apos = rels_doc.search('//rdf:RDF/rdf:Description/hydra:isGovernedBy', 'hydra' => 'http://projecthydra.org/ns/relations#', 'fedora' => 'info:fedora/fedora-system:def/relations-external#', 'rdf' => 'http://www.w3.org/1999/02/22-rdf-syntax-ns#' )
       apos.each do |apo_node|
-        druid=apo_node['rdf:resource']
-        if druid
-          druid=druid.gsub('info:fedora/','')
-          if @@apo_hash.has_key?(druid) || @@hydrus_apo_hash.has_key?(druid)
-            add_solr_value(solr_doc, "hydrus_apo_title", @@hydrus_apo_hash[druid], :string, [:searchable, :facetable]) if @@hydrus_apo_hash.has_key? druid
-            add_solr_value(solr_doc, "apo_title", @@apo_hash[druid] , :string, [:searchable, :facetable]) if @@apo_hash.has_key? druid
-          else
-            begin
-              apo_object=Dor.find(druid)
-              if apo_object.tags.include? 'Project : Hydrus'
-                add_solr_value(solr_doc, "hydrus_apo_title", apo_object.label, :string, [:searchable, :facetable])
-                @@hydrus_apo_hash[druid]=apo_object.label
-              else
-                add_solr_value(solr_doc, "apo_title", apo_object.label, :string, [:searchable, :facetable])
-                @@apo_hash[druid]=apo_object.label
-              end
-            rescue
-              add_solr_value(solr_doc, "apo_title", druid, :string, [:searchable, :facetable])
+        druid = apo_node['rdf:resource']
+        next unless druid
+        druid = druid.gsub('info:fedora/','')
+        if @@apo_hash.has_key?(druid) || @@hydrus_apo_hash.has_key?(druid)
+          add_solr_value(solr_doc, 'hydrus_apo_title', @@hydrus_apo_hash[druid], :string, [:searchable, :facetable]) if @@hydrus_apo_hash.has_key? druid
+          add_solr_value(solr_doc, 'apo_title', @@apo_hash[druid] , :string, [:searchable, :facetable]) if @@apo_hash.has_key? druid
+        else
+          begin
+            apo_object = Dor.find(druid)
+            if apo_object.tags.include? 'Project : Hydrus'
+              add_solr_value(solr_doc, 'hydrus_apo_title', apo_object.label, :string, [:searchable, :facetable])
+              @@hydrus_apo_hash[druid] = apo_object.label
+            else
+              add_solr_value(solr_doc, 'apo_title', apo_object.label, :string, [:searchable, :facetable])
+              @@apo_hash[druid] = apo_object.label
             end
+          rescue
+            add_solr_value(solr_doc, 'apo_title', druid, :string, [:searchable, :facetable])
           end
         end
       end
-      collections=rels_doc.search('//rdf:RDF/rdf:Description/fedora:isMemberOfCollection','fedora' => 'info:fedora/fedora-system:def/relations-external#', 'rdf' => 'http://www.w3.org/1999/02/22-rdf-syntax-ns#' 	)
+      collections = rels_doc.search('//rdf:RDF/rdf:Description/fedora:isMemberOfCollection', 'fedora' => 'info:fedora/fedora-system:def/relations-external#', 'rdf' => 'http://www.w3.org/1999/02/22-rdf-syntax-ns#')
       collections.each do |collection_node|
-        druid=collection_node['rdf:resource']
-        if(druid)
-          druid=druid.gsub('info:fedora/','')
-          if @@collection_hash.has_key?(druid) || @@hydrus_collection_hash.has_key?(druid)
-            add_solr_value(solr_doc, "hydrus_collection_title", @@hydrus_collection_hash[druid], :string, [:searchable, :facetable]) if @@hydrus_collection_hash.has_key? druid
-            add_solr_value(solr_doc, "collection_title", @@collection_hash[druid], :string, [:searchable, :facetable]) if @@collection_hash.has_key? druid
-          else
-            begin
-              collection_object=Dor.find(druid)
-              if collection_object.tags.include? 'Project : Hydrus'
-                add_solr_value(solr_doc, "hydrus_collection_title", collection_object.label, :string, [:searchable, :facetable])
-                @@hydrus_collection_hash[druid]=collection_object.label
-              else
-                add_solr_value(solr_doc, "collection_title", collection_object.label, :string, [:searchable, :facetable])
-                @@collection_hash[druid]=collection_object.label
-              end
-            rescue
-              add_solr_value(solr_doc, "collection_title", druid, :string, [:searchable, :facetable])
+        druid = collection_node['rdf:resource']
+        next unless druid
+        druid = druid.gsub('info:fedora/', '')
+        if @@collection_hash.has_key?(druid) || @@hydrus_collection_hash.has_key?(druid)
+          add_solr_value(solr_doc, 'hydrus_collection_title', @@hydrus_collection_hash[druid], :string, [:searchable, :facetable]) if @@hydrus_collection_hash.has_key? druid
+          add_solr_value(solr_doc, 'collection_title', @@collection_hash[druid], :string, [:searchable, :facetable]) if @@collection_hash.has_key? druid
+        else
+          begin
+            collection_object = Dor.find(druid)
+            if collection_object.tags.include? 'Project : Hydrus'
+              add_solr_value(solr_doc, 'hydrus_collection_title', collection_object.label, :string, [:searchable, :facetable])
+              @@hydrus_collection_hash[druid] = collection_object.label
+            else
+              add_solr_value(solr_doc, 'collection_title', collection_object.label, :string, [:searchable, :facetable])
+              @@collection_hash[druid] = collection_object.label
             end
+          rescue
+            add_solr_value(solr_doc, 'collection_title', druid, :string, [:searchable, :facetable])
           end
         end
       end
       # Fix for ActiveFedora 3.3 to ensure all date fields are properly formatted as UTC XML Schema datetime strings
-      solr_doc.each_pair { |k,v|
-        if k =~ /(_dt|_date)$/
-          if v.is_a?(Array)
-            solr_doc[k] = v.collect { |t| Time.parse(t.to_s).utc.xmlschema }
-          else
-            solr_doc[k] = Time.parse(v.to_s).utc.xmlschema
-          end
+      solr_doc.each_pair { |k, v|
+        next unless k =~ /(_dt|_date)$/
+        if v.is_a?(Array)
+          solr_doc[k] = v.collect { |t| Time.parse(t.to_s).utc.xmlschema }
+        else
+          solr_doc[k] = Time.parse(v.to_s).utc.xmlschema
         end
       }
-
       solr_doc
     end
+
     def set_source_id(source_id)
       identityMetadata.sourceId = source_id
     end
 
-    def add_other_Id(type,val)
-      if identityMetadata.otherId(type).length>0
+    def add_other_Id(type, val)
+      if identityMetadata.otherId(type).length > 0
         raise 'There is an existing entry for '+node_name+', consider using update_other_identifier.'
       end
       identity_metadata_ds = identityMetadata
       identity_metadata_ds.add_otherId(type+':'+val)
     end
 
-    def update_other_Id(type,new_val, val = nil)
+    def update_other_Id(type, new_val, val = nil)
       identity_metadata_ds = identityMetadata
-      ds_xml=identity_metadata_ds.ng_xml
-      #split the thing they sent in to find the node name
-      updated=false
+      ds_xml = identity_metadata_ds.ng_xml
+      # split the thing they sent in to find the node name
+      updated = false
       ds_xml.search('//otherId[@name=\''+type+'\']').each do |node|
-        if node.content==val || val.nil?
-          node.content=new_val
-          updated=true
-        end
+        next unless node.content == val || val.nil?
+        node.content = new_val
+        updated = true
       end
       updated
     end
 
-    def remove_other_Id(type,val = nil)
-      ds_xml=identityMetadata.ng_xml
-      #split the thing they sent in to find the node name
-      removed=false
-
+    def remove_other_Id(type, val = nil)
+      ds_xml = identityMetadata.ng_xml
+      # split the thing they sent in to find the node name
+      removed = false
       ds_xml.search('//otherId[@name=\''+type+'\']').each do |node|
-        if node.content===val || val.nil?
-          node.remove
-          removed=true
-        end
+        next unless node.content === val || val.nil?
+        node.remove
+        removed = true
       end
       removed
     end
@@ -174,7 +165,7 @@ module Dor
     # turns a tag string into an array with one element per tag part.
     # split on ":", disregard leading and trailing whitespace on tokens.
     def split_tag_to_arr(tag_str)
-      tag_str.split(":").map {|str| str.strip}
+      tag_str.split(':').map {|str| str.strip}
     end
 
     # turn a tag array back into a tag string with a standard format
@@ -192,45 +183,30 @@ module Dor
     # explanatory message.
     def validate_and_normalize_tag(tag_str, existing_tag_list)
       tag_arr = validate_tag_format(tag_str)
-
       # note that the comparison for duplicate tags is case-insensitive, but we don't change case as part of the normalized version
       # we return, because we want to preserve the user's intended case.
       normalized_tag = normalize_tag_arr(tag_arr)
       dupe_existing_tag = existing_tag_list.detect { |existing_tag| normalize_tag(existing_tag).downcase == normalized_tag.downcase }
-      if dupe_existing_tag
-        raise "An existing tag (#{dupe_existing_tag}) is the same, consider using update_tag?"
-      end
-
+      raise "An existing tag (#{dupe_existing_tag}) is the same, consider using update_tag?" if dupe_existing_tag
       normalized_tag
     end
 
-    #Ensure that an administrative tag meets the proper mininum format
-    #
-    #@params tag_str [String] the tag
-    #
-    #@return [Array] the tag split into an array via ':'
+    # Ensure that an administrative tag meets the proper mininum format
+    # @param [String] tag_str the tag
+    # @return [Array] the tag split into an array via ':'
     def validate_tag_format(tag_str)
       tag_arr = split_tag_to_arr(tag_str)
-
-      if tag_arr.length < 2
-        raise "Invalid tag structure:  tag '#{tag_str}' must have at least 2 elements"
-      end
-
-      if tag_arr.detect {|str| str.empty?}
-        raise "Invalid tag structure:  tag '#{tag_str}' contains empty elements"
-      end
-
+      raise "Invalid tag structure:  tag '#{tag_str}' must have at least 2 elements" if tag_arr.length < 2
+      raise "Invalid tag structure:  tag '#{tag_str}' contains empty elements" if tag_arr.detect {|str| str.empty?}
       tag_arr
-
     end
 
-    #Add an administrative tag to an item, you will need to seperately save the item to write it to fedora
-    #
-    #@param tag [string] The tag you wish to add
+    # Add an administrative tag to an item, you will need to seperately save the item to write it to fedora
+    # @param tag [string] The tag you wish to add
     def add_tag(tag)
-        identity_metadata_ds = identityMetadata
-        normalized_tag = validate_and_normalize_tag(tag, identity_metadata_ds.tags)
-        identity_metadata_ds.add_value(:tag, normalized_tag)
+      identity_metadata_ds = identityMetadata
+      normalized_tag = validate_and_normalize_tag(tag, identity_metadata_ds.tags)
+      identity_metadata_ds.add_value(:tag, normalized_tag)
     end
 
     def remove_tag(tag)
@@ -246,14 +222,13 @@ module Dor
       removed
     end
 
-    #Removes all displayTypes from an item in preparation of adding a new display type
-    #
-    #@return Boolean True if displayTypes were removed, False if no displayTypes were removed
+    # Removes all displayTypes from an item in preparation of adding a new display type
+    # @return Boolean True if displayTypes were removed, False if no displayTypes were removed
     def remove_displayTypes
       removed = false
       identityMetadata.ng_xml.search('//displayType').each do |node|
-          node.remove
-          removed = true
+        node.remove
+        removed = true
       end
       removed
     end
@@ -263,10 +238,9 @@ module Dor
       ds_xml = identity_metadata_ds.ng_xml
       updated = false
       ds_xml.search('//tag').each do |node|
-        if normalize_tag(node.content) == normalize_tag(old_tag)
-          node.content = normalize_tag(new_tag)
-          updated = true
-        end
+        next unless normalize_tag(node.content) == normalize_tag(old_tag)
+        node.content = normalize_tag(new_tag)
+        updated = true
       end
       updated
     end
