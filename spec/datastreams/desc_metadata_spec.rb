3fba398a1d4b8b963a144be073247ffcddb02f24
Joe Atzberger <ohiocore@gmail.com>
2015-10-23 16:26:56 -0700

    Write tests for DescMetadataDS

--- spec/datastreams/desc_metadata_spec.rb
+++ spec/datastreams/desc_metadata_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Dor::DescMetadataDS do
   before :each do
-    @dsxml = <<-EOF
+    @dsdoc = Dor::DescMetadataDS.from_xml <<-EOF
       <mods xmlns="http://www.loc.gov/mods/v3"
         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" version="3.3"
         xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-3.xsd">
@@ -22,16 +22,34 @@ describe Dor::DescMetadataDS do
         </language>
       </mods>
     EOF
-    @dsdoc = Dor::DescMetadataDS.from_xml(@dsxml)
+    @partial = Dor::DescMetadataDS.from_xml <<-EOF
+      <mods xmlns="http://www.loc.gov/mods/v3"
+        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" version="3.3"
+        xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-3.xsd">
+        <titleInfo>
+          <title>Electronic Theses and Dissertations</title>
+        </titleInfo>
+        <name type="corporate">
+          <namePart>Stanford University Libraries, Stanford Digital Repository</namePart>
+          <role>
+            <roleTerm authority="marcrelator" type="text">creator</roleTerm>
+          </role>
+        </name>
+        <typeOfResource collection="yes"/>
+      </mods>
+    EOF
   end
 
-  context 'Marshalling to and from a Fedora Datastream' do
-    it 'should get correct values from OM terminology' do
-      expect(@dsdoc.term_values(:abstract)).to eq ['Abstract contents.']
-      expect(@dsdoc.abstract).to eq ['Abstract contents.']
-      expect(@dsdoc.title_info.main_title).to eq ['Electronic Theses and Dissertations']
-      expect(@dsdoc.language.languageTerm).to eq ['eng']
-    end
+  it 'should get correct values from OM terminology' do
+    expect(@dsdoc.term_values(:abstract)).to eq ['Abstract contents.']
+    expect(@dsdoc.abstract).to eq ['Abstract contents.']
+    expect(@dsdoc.title_info.main_title).to eq ['Electronic Theses and Dissertations']
+    expect(@dsdoc.language.languageTerm).to eq ['eng']
+  end
+  it 'writing elements via OM terms should produce correct XML' do
+    @partial.language.languageTerm = 'eng'
+    @partial.abstract = 'Abstract contents.'
+    expect(@partial.to_xml).to be_equivalent_to(@dsdoc.to_xml)
   end
 
 end
