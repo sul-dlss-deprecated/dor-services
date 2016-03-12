27f397af379dce29baa26653670cfa4d5da397d1
Naomi Dushay <ndushay@stanford.edu>
2016-03-11 20:49:35 -0800

    rubocop fix:  alignment

--- lib/dor/models/identifiable.rb
+++ lib/dor/models/identifiable.rb
@@ -210,10 +210,11 @@ module Dor
 
     def get_related_obj_display_title(related_obj, default_title)
       # desc_md_ds_title will be nil if related_obj is nil
-      desc_md_ds_title = if related_obj
-        desc_md_ds = related_obj.datastreams['descMetadata']
-        desc_md_ds ? desc_md_ds.title_info.main_title.first : nil
-      end
+      desc_md_ds_title =
+        if related_obj
+          desc_md_ds = related_obj.datastreams['descMetadata']
+          desc_md_ds ? desc_md_ds.title_info.main_title.first : nil
+        end
 
       desc_md_ds_title.present? ? desc_md_ds_title : default_title
     end
