67a15a69f10bb36eac2d7d5740eedcb9b3609626
Naomi Dushay <ndushay@stanford.edu>
2016-03-11 19:19:37 -0800

    rubocop fixes: remove space before commas

--- lib/dor/models/describable.rb
+++ lib/dor/models/describable.rb
@@ -244,7 +244,7 @@ module Dor
           title = node.text
         end
         creator_title = creator + title
-        add_solr_value(solr_doc, 'creator_title', creator_title , :string, [:stored_sortable])
+        add_solr_value(solr_doc, 'creator_title', creator_title, :string, [:stored_sortable])
       rescue CrosswalkError => e
         ActiveFedora.logger.warn "Cannot index #{pid}.descMetadata: #{e.message}"
       end
