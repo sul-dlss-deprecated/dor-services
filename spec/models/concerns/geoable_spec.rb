3040e70bea8dbbe5e654d8f00dbe76fec86d741e
Chris Beer <chris@cbeer.info>
2016-12-02 11:47:12 -0800

    Remove unused geoable methods

--- spec/models/concerns/geoable_spec.rb
+++ spec/models/concerns/geoable_spec.rb
@@ -2,7 +2,6 @@ require 'spec_helper'
 
 class GeoableItem < ActiveFedora::Base
   include Dor::Identifiable
-  include Dor::Describable
   include Dor::Geoable
 end
 
@@ -14,18 +13,8 @@ describe Dor::Geoable do
     @item = GeoableItem.new
   end
 
-  it 'should have a descMetadata datastream' do
-    expect(@item.datastreams['descMetadata']).to be_a(Dor::DescMetadataDS)
-  end
-
   it 'should have a geoMetadata datastream' do
     expect(@item.datastreams['geoMetadata']).to be_a(Dor::GeoMetadataDS)
-  end
-
-  it 'expected methods' do
-    %w(build_geoMetadata_datastream fetch_geoMetadata_datastream).each do |k|
-      expect(@item.public_methods).to include(k.to_sym)
-    end
   end
 
   it 'expected constants' do
