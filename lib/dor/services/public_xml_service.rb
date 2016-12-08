d056109085835d21b6284ed21207e0007b9b5742
Chris Beer <chris@cbeer.info>
2016-12-08 09:40:02 -0800

    Refactor PublicXmlService#to_xml to extract methods for generating public-appropriate datastreams

--- lib/dor/services/public_xml_service.rb
+++ lib/dor/services/public_xml_service.rb
@@ -11,42 +11,57 @@ module Dor
       pub['id'] = object.pid
       pub['published'] = Time.now.utc.xmlschema
       pub['publishVersion'] = 'dor-services/' + Dor::VERSION
-      release_xml = Nokogiri(generate_release_xml)
 
-      im = object.datastreams['identityMetadata'].ng_xml.clone
-      im.search('//release').each(&:remove) # remove any <release> tags from public xml which have full history
 
-      pub.add_child(im.root) # add in modified identityMetadata datastream
+      pub.add_child(public_identity_metadata.root) # add in modified identityMetadata datastream
       public_content_metadata = object.datastreams['contentMetadata'].public_xml
       pub.add_child(public_content_metadata.root.clone) if public_content_metadata.xpath('//resource').any?
-      pub.add_child(object.datastreams['rightsMetadata'].ng_xml.root.clone)
-
-      rels = object.public_relationships.root
-      pub.add_child(rels.clone) unless rels.nil? # TODO: Should never be nil in practice; working around an ActiveFedora quirk for testing
-      pub.add_child(object.generate_dublin_core.root.clone)
-      pub.add_child(Nokogiri::XML(object.generate_public_desc_md).root.clone)
-      pub.add_child(release_xml.root.clone) unless release_xml.xpath('//release').children.size == 0 # If there are no release_tags, this prevents an empty <releaseData/> from being added
+      pub.add_child(public_rights_metadata.root)
+      pub.add_child(public_relationships.root) unless public_relationships.nil? # TODO: Should never be nil in practice; working around an ActiveFedora quirk for testing
+      pub.add_child(object.generate_dublin_core.root)
+      pub.add_child(Nokogiri::XML(object.generate_public_desc_md).root)
+      pub.add_child(release_xml.root) unless release_xml.xpath('//release').children.size == 0 # If there are no release_tags, this prevents an empty <releaseData/> from being added
       # Note we cannot base this on if an individual object has release tags or not, because the collection may cause one to be generated for an item,
       # so we need to calculate it and then look at the final result.s
-      pub.add_child(Nokogiri("<thumb>#{object.thumb}</thumb>").root.clone) unless object.thumb.nil?
+      pub.add_child(Nokogiri("<thumb>#{object.thumb}</thumb>").root) unless object.thumb.nil?
 
       new_pub = Nokogiri::XML(pub.to_xml) { |x| x.noblanks }
       new_pub.encoding = 'UTF-8'
       new_pub.to_xml
     end
-    
 
     # Generate XML structure for inclusion to Purl
     # @return [String] The XML release node as a string, with ReleaseDigest as the root document
-    def generate_release_xml
-      builder = Nokogiri::XML::Builder.new do |xml|
-        xml.releaseData {
-          object.released_for.each do |project, released_value|
-            xml.release(released_value['release'], :to => project)
-          end
-        }
+    def release_xml
+      @release_xml ||= begin
+          builder = Nokogiri::XML::Builder.new do |xml|
+          xml.releaseData {
+            object.released_for.each do |project, released_value|
+              xml.release(released_value['release'], :to => project)
+            end
+          }
+        end
+        Nokogiri::XML(builder.to_xml)
       end
-      builder.to_xml
+    end
+
+    def public_relationships
+      @public_relationships ||= object.public_relationships.clone
+    end
+
+    def public_rights_metadata
+      @public_rights_metadata ||= object.datastreams['rightsMetadata'].ng_xml.clone
+    end
+
+    def public_identity_metadata
+      @public_identity_metadata ||= begin
+        im = object.datastreams['identityMetadata'].ng_xml.clone
+        im.search('//release').each(&:remove) # remove any <release> tags from public xml which have full history
+        im
+      end
+    end
+
+      end
     end
   end
 end
