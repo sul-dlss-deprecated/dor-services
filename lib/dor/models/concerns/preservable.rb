5e61cd8f9f3a5a3816ac5cc285345d0e7f7c2bcf
Justin Coyne <jcoyne@justincoyne.com>
2018-12-05 14:50:37 -0600

    Allow collections to be preservable
    This was lost in an attempt to ensure APOs didn't have technical metadata
    (see sul-dlss/hydrus#161, #353)
    
    Now again make all objects Preservable, but move the techMetadata datastream just to Dor::Item as it is the only class of items that should have techMetadata.

--- lib/dor/models/concerns/preservable.rb
+++ lib/dor/models/concerns/preservable.rb
@@ -1,14 +1,11 @@
 # frozen_string_literal: true
 
-require 'tmpdir'
-
 module Dor
   module Preservable
     extend ActiveSupport::Concern
 
     included do
       has_metadata name: 'provenanceMetadata', type: ProvenanceMetadataDS, label: 'Provenance Metadata'
-      has_metadata name: 'technicalMetadata', type: TechnicalMetadataDS, label: 'Technical Metadata', control_group: 'M'
     end
 
     def build_provenanceMetadata_datastream(workflow_id, event_text)
@@ -19,10 +16,6 @@ module Dor
       ds.save
     end
 
-    def build_technicalMetadata_datastream(ds = nil)
-      TechnicalMetadataService.add_update_technical_metadata(self) if self.is_a?(Dor::Item) # only items need technical metadata, other object types do not have contentMetadata or content
-    end
-
     def sdr_ingest_transfer(agreement_id)
       SdrIngestService.transfer(self, agreement_id)
     end
@@ -30,15 +23,15 @@ module Dor
     # @return [Nokogiri::Document]
     def create_workflow_provenance(workflow_id, event_text)
       builder = Nokogiri::XML::Builder.new do |xml|
-        xml.provenanceMetadata(:objectId => pid) {
-          xml.agent(:name => 'DOR') {
-            xml.what(:object => pid) {
-              xml.event(:who => "DOR-#{workflow_id}", :when => Time.new.iso8601) {
+        xml.provenanceMetadata(objectId: pid) do
+          xml.agent(name: 'DOR') do
+            xml.what(object: pid) do
+              xml.event(who: "DOR-#{workflow_id}", when: Time.new.iso8601) do
                 xml.text(event_text)
-              }
-            }
-          }
-        }
+              end
+            end
+          end
+        end
       end
       builder.doc
     end
