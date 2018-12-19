f153f525d5a5eea3b534c21806efeeb5d3f0c7cf
Justin Coyne <jcoyne@justincoyne.com>
2018-12-19 10:32:50 -0600

    Deprecate Eventable#add_event

--- lib/dor/models/concerns/versionable.rb
+++ lib/dor/models/concerns/versionable.rb
@@ -38,7 +38,7 @@ module Dor
       vmd_upd_info = opts[:vers_md_upd_info]
       return unless vmd_upd_info
 
-      add_event('open', vmd_upd_info[:opening_user_name], "Version #{vmd_ds.current_version_id} opened")
+      events.add_event('open', vmd_upd_info[:opening_user_name], "Version #{vmd_ds.current_version_id} opened")
       vmd_ds.update_current_version(description: vmd_upd_info[:description], significance: vmd_upd_info[:significance].to_sym)
       save
     end
