b03ca8e97859cbb15a5fa65998c6b5b9b58fb9af
Justin Coyne <jcoyne@justincoyne.com>
2019-01-08 07:41:35 -0600

    Add key to Dor::Ontology::Term

--- spec/services/creative_commons_license_service_spec.rb
+++ spec/services/creative_commons_license_service_spec.rb
@@ -18,8 +18,11 @@ RSpec.describe Dor::CreativeCommonsLicenseService do
   end
 
   describe '#property' do
-    it 'returns terms' do
-      expect(described_class.property('by_sa').deprecation_warning).to match(/typo/)
+    subject(:property) { described_class.property('by_sa') }
+
+    it 'returns a term' do
+      expect(property.deprecation_warning).to match(/typo/)
+      expect(property.key).to eq 'by_sa'
     end
   end
 end
