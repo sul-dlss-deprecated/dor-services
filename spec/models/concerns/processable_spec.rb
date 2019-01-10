bbe8f01d044cf8efb0faaa9346dd80b6042c2d1a
Justin Coyne <jcoyne@justincoyne.com>
2019-01-09 20:14:27 -0600

    Don't use instance variables in tests

--- spec/models/concerns/processable_spec.rb
+++ spec/models/concerns/processable_spec.rb
@@ -17,35 +17,36 @@ end
 class ProcessableWithApoItem < ActiveFedora::Base
   include Dor::Governable
   include Dor::Processable
+  include Dor::Itemizable
 end
 
 RSpec.describe Dor::Processable do
   after { unstub_config }
 
+  let(:item) { instantiate_fixture('druid:ab123cd4567', ProcessableItem) }
   before do
     stub_config
-    @item = instantiate_fixture('druid:ab123cd4567', ProcessableItem)
-    @item.contentMetadata.content = '<contentMetadata/>'
+    item.contentMetadata.content = '<contentMetadata/>'
   end
 
   it 'has a workflows datastream and workflows shortcut method' do
-    expect(@item.datastreams['workflows']).to be_a(Dor::WorkflowDs)
-    expect(@item.workflows).to eq(@item.datastreams['workflows'])
+    expect(item.datastreams['workflows']).to be_a(Dor::WorkflowDs)
+    expect(item.workflows).to eq(item.datastreams['workflows'])
   end
 
   it 'should load its content directly from the workflow service' do
     expect(Dor::Config.workflow.client).to receive(:get_workflow_xml).with('dor', 'druid:ab123cd4567', nil).once { '<workflows/>' }
-    expect(@item.workflows.content).to eq('<workflows/>')
+    expect(item.workflows.content).to eq('<workflows/>')
   end
 
   it 'should be able to invalidate the cache of its content' do
     expect(Dor::Config.workflow.client).to receive(:get_workflow_xml).with('dor', 'druid:ab123cd4567', nil).once { '<workflows/>' }
-    expect(@item.workflows.content).to eq('<workflows/>')
-    expect(@item.workflows.content).to eq('<workflows/>') # should be cached copy
+    expect(item.workflows.content).to eq('<workflows/>')
+    expect(item.workflows.content).to eq('<workflows/>') # should be cached copy
     expect(Dor::Config.workflow.client).to receive(:get_workflow_xml).with('dor', 'druid:ab123cd4567', nil).once { '<workflows>with some data</workflows>' }
     # pass refresh flag and should be refreshed copy
-    expect(@item.workflows.content(true)).to eq('<workflows>with some data</workflows>')
-    expect(@item.workflows.content).to eq('<workflows>with some data</workflows>')
+    expect(item.workflows.content(true)).to eq('<workflows>with some data</workflows>')
+    expect(item.workflows.content).to eq('<workflows>with some data</workflows>')
   end
 
   describe '#build_datastream' do
@@ -54,9 +55,9 @@ RSpec.describe Dor::Processable do
     it 'Calls the datastream builder' do
       expect(Deprecation).to receive(:warn)
       expect(Dor::DatastreamBuilder).to receive(:new)
-        .with(datastream: Dor::DescMetadataDS, force: true, object: @item, required: false)
+        .with(datastream: Dor::DescMetadataDS, force: true, object: item, required: false)
         .and_return(builder)
-      @item.build_datastream('descMetadata', true)
+      item.build_datastream('descMetadata', true)
       expect(builder).to have_received(:build)
     end
   end
@@ -64,8 +65,8 @@ RSpec.describe Dor::Processable do
   describe '#status' do
     it 'delegates to the StatusService' do
       expect(Deprecation).to receive(:warn)
-      expect(Dor::StatusService).to receive(:status).with(@item, true)
-      @item.status(true)
+      expect(Dor::StatusService).to receive(:status).with(item, true)
+      item.status(true)
     end
   end
 
