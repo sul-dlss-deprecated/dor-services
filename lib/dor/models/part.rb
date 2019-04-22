b0eeb87002f4f11db09298a5586bd3e5a8541dca
Justin Coyne <jcoyne@justincoyne.com>
2019-04-22 08:32:45 -0500

    Move the Etd/Part model in from etd_models
    This will make it easier as all models in DOR will be in one codebase

---
+++ lib/dor/models/part.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Dor
+  # Represents a piece of an ETD submission
+  class Part < ActiveFedora::Base
+    belongs_to :parents, property: :is_part_of, class_name: 'Etd' # relationship between main pdf and parent etd
+    belongs_to :supplemental_file_for, property: :is_constituent_of, class_name: 'Etd' # relationship between supplemental file and parent etd
+    belongs_to :permission_file_for, property: :is_dependent_of, class_name: 'Etd' # relationsihip between permission file and parent etd
+
+    has_metadata name: 'properties', type: ActiveFedora::SimpleDatastream, versionable: false do |m|
+      m.field 'file_name', :string
+      m.field 'size', :string
+      m.field 'label', :string
+    end
+
+    has_metadata name: 'DC', type: Dor::SimpleDublinCoreDs, versionable: false, label: 'DC', control_group: 'X'
+  end
+end
