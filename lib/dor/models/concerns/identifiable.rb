50d6db730ad8316dd42a79ba72da3cbfb0ce090b
Justin Coyne <jcoyne@justincoyne.com>
2019-04-22 21:26:27 -0500

    Move methods that are about the object to Dor::Abstract

--- lib/dor/models/concerns/identifiable.rb
+++ lib/dor/models/concerns/identifiable.rb
@@ -8,27 +8,6 @@ module Dor
     CATKEY_TYPE_ID = 'catkey'
     PREVIOUS_CATKEY_TYPE_ID = 'previous_catkey'
 
-    included do
-      has_metadata name: 'DC', type: SimpleDublinCoreDs, label: 'Dublin Core Record for self object'
-      has_metadata name: 'identityMetadata', type: Dor::IdentityMetadataDS, label: 'Identity Metadata'
-    end
-
-    module ClassMethods
-      attr_reader :object_type
-      def has_object_type(str)
-        @object_type = str
-        Dor.registered_classes[str] = self
-      end
-
-      # Overrides the method in ActiveFedora to mint a pid using SURI rather
-      # than the default Fedora sequence
-      def assign_pid(_obj)
-        return Dor::SuriService.mint_id if Dor::Config.suri.mint_ids
-
-        super
-      end
-    end
-
     # helper method to get the tags as an array
     def tags
       identityMetadata.tag
@@ -103,42 +82,6 @@ module Dor
                       .each { identityMetadata.ng_xml_will_change! }
                       .each(&:remove)
                       .any?
-    end
-
-    # a regex that can be used to identify the last part of a druid (e.g. oo000oo0001)
-    # @return [Regex] a regular expression to identify the ID part of the druid
-    def pid_regex
-      /[a-zA-Z]{2}[0-9]{3}[a-zA-Z]{2}[0-9]{4}/
-    end
-
-    # a regex that can be used to identify a full druid with prefix (e.g. druid:oo000oo0001)
-    # @return [Regex] a regular expression to identify a full druid
-    def druid_regex
-      /druid:#{pid_regex}/
-    end
-
-    # Since purl does not use the druid: prefix but much of dor does, use this function to strip the druid: if needed
-    # @return [String] the druid sans the druid: or if there was no druid: prefix, the entire string you passed
-    def remove_druid_prefix(druid = id)
-      result = druid.match(/#{pid_regex}/)
-      result.nil? ? druid : result[0] # if no matches, return the string passed in, otherwise return the match
-    end
-
-    # Override ActiveFedora::Core#adapt_to_cmodel (used with associations, among other places) to
-    # preferentially use the objectType asserted in the identityMetadata.
-    def adapt_to_cmodel
-      object_type = identityMetadata.objectType.first
-      object_class = Dor.registered_classes[object_type]
-
-      if object_class
-        instance_of?(object_class) ? self : adapt_to(object_class)
-      else
-        begin
-          super
-        rescue ActiveFedora::ModelNotAsserted
-          adapt_to(Dor::Item)
-        end
-      end
     end
   end
 end
